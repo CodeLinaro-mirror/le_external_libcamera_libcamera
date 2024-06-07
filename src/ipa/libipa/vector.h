f70ddc98aaf34dc2bda028329435a038759476a4
Paul Elder <paul.elder@ideasonboard.com>
2024-06-07 16:50:58 +0900

    ipa: libipa: vector: Add matrix-vector multiplication
    Add an operation for multiplying a matrix with a vector.
    
    Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    
    ---
    Changes in v3:
    - make SFINAE more concise
    
    Changes in v2:
    - s/D/Rows/ in Vector class
    
    Depends on v9 of "ipa: libipa: Add Matrix class"

--- src/ipa/libipa/vector.h
+++ src/ipa/libipa/vector.h
@@ -16,6 +16,8 @@
 
 #include "libcamera/internal/yaml_parser.h"
 
+#include "matrix.h"
+
 namespace libcamera {
 
 LOG_DECLARE_CATEGORY(Vector)
@@ -139,6 +141,23 @@ public:
 private:
 	std::array<T, Rows> data_;
 };
+
+#ifndef __DOXYGEN__
+template<typename T, unsigned int Rows, unsigned int Cols>
+#endif /* __DOXYGEN__ */
+Vector<T, Rows> operator*(const Matrix<T, Rows, Cols> &m, const Vector<T, Cols> &v)
+{
+	Vector<T, Rows> result;
+
+	for (unsigned int i = 0; i < Rows; i++) {
+		T sum = 0;
+		for (unsigned int j = 0; j < Cols; j++)
+			sum += m[i][j] * v[j];
+		result[i] = sum;
+	}
+
+	return result;
+}
 
 template<typename T, unsigned int Rows>
 bool operator==(const Vector<T, Rows> &lhs, const Vector<T, Rows> &rhs)
