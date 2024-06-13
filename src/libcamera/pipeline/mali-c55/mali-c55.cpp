3f996a7fa05a45099faffdc5e3e2b53aef64e98f
Daniel Scally <dan.scally@ideasonboard.com>
2024-06-13 16:59:48 +0100

    libcamera: mali-c55: Remove MaliC55CameraData::mbusCodes()
    The MaliC55CameraData::mbusCodes() function is unused. Remove it.
    
    Signed-off-by: Daniel Scally <dan.scally@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/mali-c55/mali-c55.cpp
+++ src/libcamera/pipeline/mali-c55/mali-c55.cpp
@@ -94,7 +94,6 @@ public:
 	int init();
 
 	/* Deflect these functionalities to either TPG or CameraSensor. */
-	const std::vector<unsigned int> mbusCodes() const;
 	const std::vector<Size> sizes(unsigned int mbusCode) const;
 	const Size resolution() const;
 
@@ -177,14 +176,6 @@ void MaliC55CameraData::initTPGData()
 	}
 
 	tpgResolution_ = tpgSizes_.back();
-}
-
-const std::vector<unsigned int> MaliC55CameraData::mbusCodes() const
-{
-	if (sensor_)
-		return sensor_->mbusCodes();
-
-	return tpgCodes_;
 }
 
 const std::vector<Size> MaliC55CameraData::sizes(unsigned int mbusCode) const
