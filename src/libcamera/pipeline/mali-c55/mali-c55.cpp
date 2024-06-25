5efaeb08b2da816bcde9c20f4887760b02585396
Daniel Scally <dan.scally@ideasonboard.com>
2024-06-25 21:04:16 +0200

    libcamera: mali-c55: Init camera properties
    Initialise the camera properties using the CameraSensor properties.
    
    Signed-off-by: Daniel Scally <dan.scally@ideasonboard.com>
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/mali-c55/mali-c55.cpp
+++ src/libcamera/pipeline/mali-c55/mali-c55.cpp
@@ -966,7 +966,7 @@ bool PipelineHandlerMaliC55::registerSensorCamera(MediaLink *ispLink)
 		if (data->init())
 			return false;
 
-		/* \todo: Init properties and controls. */
+		data->properties_ = data->sensor_->properties();
 
 		registerMaliCamera(std::move(data), sensor->name());
 	}
