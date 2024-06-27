164054031d289f5df11e549c80b46342c46e2f9a
Milan Zamazal <mzamazal@redhat.com>
2024-06-27 16:51:38 +0200

    config: Introduce global runtime configuration
    Currently, libcamera can be configured in runtime using several
    environment variables.  With introducing more and more variables, this
    mechanism reaches its limits.  It would be simpler and more flexible if
    it was possible to configure libcamera in a single file.
    
    For example, there have been a request for defining pipeline precedence
    in runtime.  We want to compile in multiple pipelines, in order to have
    them accessible within single packages in distributions.  And then being
    able to select among the pipelines manually as needed based on the
    particular hardware or operating system environment.  Having the
    configuration file then allows easy switching between hardware, GPU or
    CPU IPAs.  Another possible use case is tuning image output, especially
    with software ISP, to user liking.  For example, some users may prefer
    higher contrast without the need to use the corresponding knobs, if
    present at all, in every application.  The configuration file can also
    be used to enable or disable experimental features and avoid the need to
    track local patches changing configuration options hard-wired in the
    code when working on new features.
    
    This patch introduces basic support for configuration files.
    GlobalConfiguration class reads, stores and accesses the configuration.
    Its instance is stored as a private singleton accessed using a static
    method of the class.  There is currently no reason to have more than one
    instance.
    
    libcamera configuration can be specified using a system-wide
    configuration file or a user configuration file.  The user configuration
    file takes precedence if present.  There is currently no way to merge
    multiple configuration files, the one found is used as the only
    configuration file.  If no configuration file is present, nothing
    changes to the current libcamera behavior (except for some log
    messages related to configuration file lookup).
    
    The configuration file is a YAML file.  We already have a mechanism for
    handling YAML configuration files in libcamera and the given
    infrastructure can be reused for the purpose.  However, the
    configuration type is abstracted to make contingent future change of the
    underlying class easier while retaining (most of) the original API.
    
    The configuration is versioned.  This has currently no particular
    meaning but is likely to have its purpose in future, especially once
    configuration validation is introduced.
    
    The configuration YAML file looks as follows:
    
      ---
      version: 1
      configuration:
        WHATEVER CONFIGURATION NEEDED
    
    This patch introduces just the basic idea.  Actually using the
    configuration in the corresponding places (everything what is currently
    configurable via environment variables should be configurable in the
    file configuration), refining the mechanism (e.g. configuration file
    validation to avoid mistakes caused by typos) and other improvements
    will be addressed in followup patches.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/libcamera/global_configuration.cpp
@@ -0,0 +1,163 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2024 Red Hat, inc.
+ *
+ * global_configuration.cpp - Global configuration handling
+ */
+
+#include "libcamera/internal/global_configuration.h"
+
+#include <filesystem>
+#include <memory>
+#include <stdint.h>
+#include <sys/types.h>
+
+#include <libcamera/base/file.h>
+#include <libcamera/base/log.h>
+#include <libcamera/base/utils.h>
+
+#include "libcamera/internal/yaml_parser.h"
+
+namespace libcamera {
+
+LOG_DEFINE_CATEGORY(Configuration)
+
+/**
+ * \class GlobalConfiguration
+ * \brief Support for global libcamera configuration
+ *
+ * The configuration file is a YAML file and the configuration itself is stored
+ * under `configuration' top-level item.
+ *
+ * The configuration file is looked up in user's home directory first and if it
+ * is not found then in system-wide configuration directories. If multiple
+ * configuration files exist then only the first one found is used and no
+ * configuration merging is performed.
+ *
+ * The class is used as a private singleton and the configuration can be
+ * accessed usingGlobalConfiguration::configuration().
+ */
+
+/**
+ * \typedef GlobalConfiguration::Configuration
+ * \brief Type representing global libcamera configuration
+ *
+ * All code outside GlobalConfiguration must use this type declaration and not
+ * the underlying type.
+ */
+
+GlobalConfiguration::GlobalConfiguration()
+	: initialized_(false), configuration_(std::make_unique<YamlObject>())
+{
+}
+
+const std::vector<std::filesystem::path>
+	GlobalConfiguration::globalConfigurationFiles = {
+		std::filesystem::path(LIBCAMERA_SYSCONF_DIR) / "configuration.yaml",
+		std::filesystem::path("/etc/libcamera/configuration.yaml"),
+	};
+
+void GlobalConfiguration::load()
+{
+	std::filesystem::path userConfigurationDirectory;
+	char *xdgConfigHome = utils::secure_getenv("XDG_CONFIG_HOME");
+	if (xdgConfigHome) {
+		userConfigurationDirectory = xdgConfigHome;
+	} else {
+		const char *home = utils::secure_getenv("HOME");
+		if (home)
+			userConfigurationDirectory =
+				std::filesystem::path(home) / ".config";
+	}
+
+	if (!userConfigurationDirectory.empty()) {
+		std::filesystem::path user_configuration_file =
+			userConfigurationDirectory / "libcamera" / "configuration.yaml";
+		if (loadFile(user_configuration_file))
+			return;
+	}
+
+	for (auto path : globalConfigurationFiles)
+		if (loadFile(path))
+			return;
+
+	initialized_ = true;
+	LOG(Configuration, Debug) << "No configuration file found";
+}
+
+bool GlobalConfiguration::loadFile(const std::filesystem::path &fileName)
+{
+	File file(fileName);
+	if (!file.exists()) {
+		return false;
+	}
+
+	if (!file.open(File::OpenModeFlag::ReadOnly)) {
+		LOG(Configuration, Warning)
+			<< "Failed to open configuration file '" << fileName << "'";
+		return true;
+	}
+
+	auto root = YamlParser::parse(file);
+	if (!root) {
+		LOG(Configuration, Warning) << "Failed to parse configuration file "
+					    << fileName;
+		return true;
+	}
+	configuration_ = std::move(root);
+	initialized_ = true;
+	LOG(Configuration, Info) << "Configuration file " << fileName << "loaded";
+
+	return true;
+}
+
+const GlobalConfiguration &GlobalConfiguration::instance()
+{
+	static GlobalConfiguration configuration;
+	if (!configuration.initialized_) {
+		configuration.load();
+	}
+	return configuration;
+}
+
+GlobalConfiguration::Configuration GlobalConfiguration::get()
+{
+	return (*instance().configuration_);
+}
+
+/**
+ * \brief Return configuration version
+ *
+ * The version is (optionally) declared in the configuration file in the
+ * top-level section `version', alongside `configuration'. This has currently no
+ * real use but may be needed in future if configuration incompatibilities
+ * occur.
+ *
+ * \return Configuration version as declared in the configuration file or 0 if
+ * no version is declared there
+ */
+unsigned int GlobalConfiguration::version()
+{
+	return get()["version"].get<unsigned int>().value_or(0);
+}
+
+/**
+ * \brief Return libcamera global configuration
+ *
+ * This returns the whole configuration stored in the top-level section
+ * `configuration' of the YAML configuration file.
+ *
+ * The requested part of the configuration can be accessed using \a YamlObject
+ * methods.
+ *
+ * \note \a YamlObject type itself shouldn't be used in type declarations to
+ * avoid trouble if we decide to change the underlying data objects in future.
+ *
+ * \return The whole configuration section
+ */
+GlobalConfiguration::Configuration GlobalConfiguration::configuration()
+{
+	return get()["configuration"];
+}
+
+} /* namespace libcamera */
