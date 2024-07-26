54bcc662d447718c1e3f61fffa1e03edc827b053
Konami Shu <konamiz@google.com>
2024-07-26 09:54:26 +0000

    libcamera: pipeline: Read config and register cameras based on the config
    - Use Yaml::Parser to parse the config
    - Add config file at "virtual/data/virtual.yaml"
    - README.md contains the documentation for the format of config file and
      the implementation of Parser class.
    - Add Parser class to parse config file in Virtual
    pipeline handler
    - Add header file of virtual.cpp to use VirtualCameraData class in
      Parser class
    - Parse test patterns
    - Raise Error when the width of a resolution is an odd number
    
    Signed-off-by: Konami Shu <konamiz@google.com>
    Co-developed-by: Harvey Yang <chenghaoyang@chromium.org>
    Co-developed-by: Yunke Cao <yunkec@chromium.org>
    Co-developed-by: Tomasz Figa <tfiga@chromium.org>
    
    Change-Id: I7f8c375e9a2dc4d06a94c06ddc0e8bc3a432dc2f
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/libcamera/pipeline/virtual/parser.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2023, Google Inc.
+ *
+ * parser.h - Virtual cameras helper to parse config file
+ */
+
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include <libcamera/base/file.h>
+
+#include "libcamera/internal/pipeline_handler.h"
+#include "libcamera/internal/yaml_parser.h"
+
+#include "virtual.h"
+
+namespace libcamera {
+
+class Parser
+{
+public:
+	Parser() {}
+	~Parser() = default;
+
+	std::vector<std::unique_ptr<VirtualCameraData>>
+	parseConfigFile(File &file, PipelineHandler *pipe);
+
+private:
+	std::unique_ptr<VirtualCameraData> parseCameraConfigData(
+		const YamlObject &cameraConfigData, PipelineHandler *pipe);
+
+	int parseSupportedFormats(
+		const YamlObject &cameraConfigData, VirtualCameraData *data);
+	int parseTestPattern(
+		const YamlObject &cameraConfigData, VirtualCameraData *data);
+	int parseLocation(
+		const YamlObject &cameraConfigData, VirtualCameraData *data);
+	int parseModel(
+		const YamlObject &cameraConfigData, VirtualCameraData *data);
+};
+
+} // namespace libcamera
