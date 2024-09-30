91093a387d7cde61ebb372cb00885e5780cb362b
Harvey Yang <chenghaoyang@chromium.org>
2024-09-30 06:29:47 +0000

    libcamera: virtual: Read config and register cameras based on the config
    This patch introduces the configuration file for Virtual Pipeline
    Handler. The config file is written in yaml, and the format is
    documented in `README.md`.
    
    The config file will define the camera with IDs, supported formats and
    image sources, etc. In the default config file, only Test Patterns are
    used. Developers can use real images loading if desired.
    
    Signed-off-by: Konami Shu <konamiz@google.com>
    Co-developed-by: Harvey Yang <chenghaoyang@chromium.org>
    Co-developed-by: Yunke Cao <yunkec@chromium.org>
    Co-developed-by: Tomasz Figa <tfiga@chromium.org>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/virtual/virtual.h
+++ src/libcamera/pipeline/virtual/virtual.h
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <string>
+#include <variant>
 #include <vector>
 
 #include <libcamera/geometry.h>
@@ -15,9 +17,13 @@
 #include "libcamera/internal/camera.h"
 #include "libcamera/internal/pipeline_handler.h"
 
+#include "frame_generator.h"
+#include "image_frame_generator.h"
 #include "test_pattern_generator.h"
 
 namespace libcamera {
+
+using VirtualFrame = std::variant<TestPattern, ImageFrames>;
 
 class VirtualCameraData : public Camera::Private
 {
@@ -32,17 +38,22 @@ public:
 		Stream stream;
 		std::unique_ptr<FrameGenerator> frameGenerator;
 	};
+	/* The config file is parsed to the Configuration struct */
+	struct Configuration {
+		std::string id;
+		std::vector<Resolution> resolutions;
+		VirtualFrame frame;
+
+		Size maxResolutionSize;
+		Size minResolutionSize;
+	};
 
 	VirtualCameraData(PipelineHandler *pipe,
 			  const std::vector<Resolution> &supportedResolutions);
 
 	~VirtualCameraData() = default;
 
-	TestPattern testPattern_ = TestPattern::ColorBars;
-
-	const std::vector<Resolution> supportedResolutions_;
-	Size maxResolutionSize_;
-	Size minResolutionSize_;
+	Configuration config_;
 
 	std::vector<StreamConfig> streamConfigs_;
 };
