6f66aa6ff798ce21a547bfa1547fb84cd72d4849
Milan Zamazal <mzamazal@redhat.com>
2024-10-01 12:28:06 +0200

    libcamera: software_isp: Make input buffer copying configurable
    On some platforms, working directly on the input buffer is very slow due
    to disabled caching.  This is why we copy the input buffer into standard
    (cached) memory.  This is an unnecessary overhead on platforms with
    cached buffers.
    
    Let's make input buffer copying configurable.  The default is still
    copying, as it's overhead is much lower than contingent operations on
    non-cached memory.  Ideally, we should improve this in future to set the
    default to non-copying if we can be sure under observable circumstances
    that we are working with cached buffers.
    
    Completes software ISP TODO #6.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/software_isp/debayer_cpu.cpp
+++ src/libcamera/software_isp/debayer_cpu.cpp
@@ -22,6 +22,7 @@
 #include "libcamera/internal/bayer_format.h"
 #include "libcamera/internal/dma_buf_allocator.h"
 #include "libcamera/internal/framebuffer.h"
+#include "libcamera/internal/global_configuration.h"
 #include "libcamera/internal/mapped_framebuffer.h"
 
 namespace libcamera {
@@ -48,7 +49,9 @@ DebayerCpu::DebayerCpu(std::unique_ptr<SwStatsCpu> stats)
 	 * always set it to true as the safer choice but this should be changed in
 	 * future.
 	 */
-	enableInputMemcpy_ = true;
+	enableInputMemcpy_ = GlobalConfiguration::option<bool>(
+				     "pipelines.simple.copy_input_buffer")
+				     .value_or(true);
 
 	/* Initialize color lookup tables */
 	for (unsigned int i = 0; i < DebayerParams::kRGBLookupSize; i++)
