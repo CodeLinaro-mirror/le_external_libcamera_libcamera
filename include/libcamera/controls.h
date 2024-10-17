raspberrypi/libcamera

commit 200d535ca85f23e6c562335c5d9badb5be4db28f

libcamera: controls: Add ControlTypePoint
Add a control_type<> specialization for libcamera::Point to allow
storing data of that type in a ControlValue instance.

The new control type will be used by controls introduced in the
next patches.

Signed-off-by: Yudhistira Erlandinata <yerlandinata@chromium.org>
Co-developed-by: Becker Hsieh <beckerh@chromium.org>
Co-developed-by: Harvey Yang <chenghaoyang@chromium.org>
Reviewed-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
Reviewed-by: Harvey Yang <chenghaoyang@chromium.org>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

include/libcamera/controls.h
@@ -35,6 +35,7 @@ enum ControlType {
 	ControlTypeString,
 	ControlTypeRectangle,
 	ControlTypeSize,
+	ControlTypePoint,
 };
 
 namespace details {
@@ -95,6 +96,11 @@ template<>
 struct control_type<Size> {
 	static constexpr ControlType value = ControlTypeSize;
 	static constexpr std::size_t size = 0;
+};
+
+template<>
+struct control_type<Point> {
+	static constexpr ControlType value = ControlTypePoint;
 };
 
 template<typename T, std::size_t N>

src/libcamera/controls.cpp
@@ -60,6 +60,7 @@ static constexpr size_t ControlValueSize[] = {
 	[ControlTypeString]		= sizeof(char),
 	[ControlTypeRectangle]		= sizeof(Rectangle),
 	[ControlTypeSize]		= sizeof(Size),
+	[ControlTypePoint]		= sizeof(Point),
 };
 
 } /* namespace */
@@ -251,6 +252,11 @@ std::string ControlValue::toString() const
 		}
 		case ControlTypeSize: {
 			const Size *value = reinterpret_cast<const Size *>(data);
+			str += value->toString();
+			break;
+		}
+		case ControlTypePoint: {
+			const Point *value = reinterpret_cast<const Point *>(data);
 			str += value->toString();
 			break;
 		}

CodeLinaro-mirror/le_external_libcamera_libcamera

commit c8a7beea7a0ba7fdbf249f76012b92754dd09e87

libcamera: controls: Add missing size to control_type<Point>
The size member is missing in control_type<Point>. Add it.

Fixes: 200d535ca85f ("libcamera: controls: Add ControlTypePoint")
Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

include/libcamera/controls.h
@@ -101,6 +101,7 @@ struct control_type<Size> {
 template<>
 struct control_type<Point> {
 	static constexpr ControlType value = ControlTypePoint;
+	static constexpr std::size_t size = 0;
 };
 
 template<typename T, std::size_t N>