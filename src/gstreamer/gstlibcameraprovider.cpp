raspberrypi/libcamera

commit 5a142438b025b448eedfa67678fe8e169c66f566

gstreamer: Add enable_auto_focus option to the GStreamer plugin
Cameras such as the PiCam 3 support auto-focus, but the GStreamer plugin
for libcamera does not enable auto-focus. With this patch auto-focus can
be enabled for cameras that support it. By default it is disabled, which
means default behaviour remains unchanged. For cameras that do not
support auto-focus, an error message shows up if auto-focus is enabled.

This was tested on cameras that do not support auto-focus (e.g. PiCam2)
and was tested on a camera that does support auto-focus (PiCam3). The
test involved setting the focus to AfModeContinous and observing it.
However, by not setting "auto-focus-mode" or using AfModeManual as
the "auto-focus-mode" both resulting in auto-focus being disabled.

Bug: https://bugs.libcamera.org/show_bug.cgi?id=188
Signed-off-by: Cedric Nugteren <cedric@plumerai.com>
Reviewed-by: Maarten Lankhorst <dev@lankhorst.se>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>
Tested-by: Umang Jain <umang.jain@ideasonboard.com>
Signed-off-by: Umang Jain <umang.jain@ideasonboard.com>

src/gstreamer/gstlibcameraprovider.cpp
@@ -31,6 +31,7 @@ GST_DEBUG_CATEGORY_STATIC(provider_debug);
 
 enum {
 	PROP_DEVICE_NAME = 1,
+	PROP_AUTO_FOCUS_MODE = 2,
 };
 
 #define GST_TYPE_LIBCAMERA_DEVICE gst_libcamera_device_get_type()
@@ -40,6 +41,7 @@ G_DECLARE_FINAL_TYPE(GstLibcameraDevice, gst_libcamera_device,
 struct _GstLibcameraDevice {
 	GstDevice parent;
 	gchar *name;
+	controls::AfModeEnum auto_focus_mode = controls::AfModeManual;
 };
 
 G_DEFINE_TYPE(GstLibcameraDevice, gst_libcamera_device, GST_TYPE_DEVICE)
@@ -56,6 +58,7 @@ gst_libcamera_device_create_element(GstDevice *device, const gchar *name)
 	g_assert(source);
 
 	g_object_set(source, "camera-name", GST_LIBCAMERA_DEVICE(device)->name, nullptr);
+	g_object_set(source, "auto-focus-mode", GST_LIBCAMERA_DEVICE(device)->auto_focus_mode, nullptr);
 
 	return source;
 }
@@ -81,6 +84,9 @@ gst_libcamera_device_set_property(GObject *object, guint prop_id,
 	switch (prop_id) {
 	case PROP_DEVICE_NAME:
 		device->name = g_value_dup_string(value);
+		break;
+	case PROP_AUTO_FOCUS_MODE:
+		device->auto_focus_mode = static_cast<controls::AfModeEnum>(g_value_get_enum(value));
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
@@ -121,6 +127,15 @@ gst_libcamera_device_class_init(GstLibcameraDeviceClass *klass)
 						(GParamFlags)(G_PARAM_STATIC_STRINGS | G_PARAM_WRITABLE |
 							      G_PARAM_CONSTRUCT_ONLY));
 	g_object_class_install_property(object_class, PROP_DEVICE_NAME, pspec);
+
+	pspec = g_param_spec_enum("auto-focus-mode",
+				  "Set auto-focus mode",
+				  "Available options: AfModeManual, "
+				  "AfModeAuto or AfModeContinuous.",
+				  gst_libcamera_auto_focus_get_type(),
+				  static_cast<gint>(controls::AfModeManual),
+				  G_PARAM_WRITABLE);
+	g_object_class_install_property(object_class, PROP_AUTO_FOCUS_MODE, pspec);
 }
 
 static GstDevice *

src/gstreamer/gstlibcamerasrc.cpp
@@ -146,6 +146,7 @@ struct _GstLibcameraSrc {
 	GstTask *task;
 
 	gchar *camera_name;
+	controls::AfModeEnum auto_focus_mode = controls::AfModeManual;
 
 	GstLibcameraSrcState *state;
 	GstLibcameraAllocator *allocator;
@@ -154,7 +155,8 @@ struct _GstLibcameraSrc {
 
 enum {
 	PROP_0,
-	PROP_CAMERA_NAME
+	PROP_CAMERA_NAME,
+	PROP_AUTO_FOCUS_MODE,
 };
 
 G_DEFINE_TYPE_WITH_CODE(GstLibcameraSrc, gst_libcamera_src, GST_TYPE_ELEMENT,
@@ -577,6 +579,18 @@ gst_libcamera_src_task_enter(GstTask *task, [[maybe_unused]] GThread *thread,
 		gst_flow_combiner_add_pad(self->flow_combiner, srcpad);
 	}
 
+	if (self->auto_focus_mode != controls::AfModeManual) {
+		const ControlInfoMap &infoMap = state->cam_->controls();
+		if (infoMap.find(&controls::AfMode) != infoMap.end()) {
+			state->initControls_.set(controls::AfMode, self->auto_focus_mode);
+		} else {
+			GST_ELEMENT_ERROR(self, RESOURCE, SETTINGS,
+					  ("Failed to enable auto focus"),
+					  ("AfMode not supported by this camera, "
+					   "please retry with 'auto-focus-mode=AfModeManual'"));
+		}
+	}
+
 	ret = state->cam_->start(&state->initControls_);
 	if (ret) {
 		GST_ELEMENT_ERROR(self, RESOURCE, SETTINGS,
@@ -659,6 +673,9 @@ gst_libcamera_src_set_property(GObject *object, guint prop_id,
 		g_free(self->camera_name);
 		self->camera_name = g_value_dup_string(value);
 		break;
+	case PROP_AUTO_FOCUS_MODE:
+		self->auto_focus_mode = static_cast<controls::AfModeEnum>(g_value_get_enum(value));
+		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
 		break;
@@ -675,6 +692,9 @@ gst_libcamera_src_get_property(GObject *object, guint prop_id, GValue *value,
 	switch (prop_id) {
 	case PROP_CAMERA_NAME:
 		g_value_set_string(value, self->camera_name);
+		break;
+	case PROP_AUTO_FOCUS_MODE:
+		g_value_set_enum(value, static_cast<gint>(self->auto_focus_mode));
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
@@ -844,4 +864,13 @@ gst_libcamera_src_class_init(GstLibcameraSrcClass *klass)
 							     | G_PARAM_READWRITE
 							     | G_PARAM_STATIC_STRINGS));
 	g_object_class_install_property(object_class, PROP_CAMERA_NAME, spec);
+
+	spec = g_param_spec_enum("auto-focus-mode",
+				 "Set auto-focus mode",
+				 "Available options: AfModeManual, "
+				 "AfModeAuto or AfModeContinuous.",
+				 gst_libcamera_auto_focus_get_type(),
+				 static_cast<gint>(controls::AfModeManual),
+				 G_PARAM_WRITABLE);
+	g_object_class_install_property(object_class, PROP_AUTO_FOCUS_MODE, spec);
 }

src/gstreamer/gstlibcamerasrc.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <libcamera/control_ids.h>
+
 #include <gst/gst.h>
 
 G_BEGIN_DECLS
@@ -17,3 +19,32 @@ G_DECLARE_FINAL_TYPE(GstLibcameraSrc, gst_libcamera_src,
 		     GST_LIBCAMERA, SRC, GstElement)
 
 G_END_DECLS
+
+inline GType
+gst_libcamera_auto_focus_get_type()
+{
+	static GType type = 0;
+	static const GEnumValue values[] = {
+		{
+			static_cast<gint>(libcamera::controls::AfModeManual),
+			"AfModeManual",
+			"manual-focus",
+		},
+		{
+			static_cast<gint>(libcamera::controls::AfModeAuto),
+			"AfModeAuto",
+			"automatic-auto-focus",
+		},
+		{
+			static_cast<gint>(libcamera::controls::AfModeContinuous),
+			"AfModeContinuous",
+			"continuous-auto-focus",
+		},
+		{ 0, NULL, NULL }
+	};
+
+	if (!type)
+		type = g_enum_register_static("GstLibcameraAutoFocus", values);
+
+	return type;
+}

CodeLinaro-mirror/le_external_libcamera_libcamera

commit b75d5cc0269d787dfb87326b894d4d22887476cd

gstreamer: Remove auto-focus-mode property from device provider
The device provider is not supposed to have control properties, remove
the auto-focus-mode property which was introduced by accident.

Fixes: 5a142438b025 ("gstreamer: Add enable_auto_focus option to the GStreamer plugin")
Signed-off-by: Jaslo Ziska <jaslo@ziska.de>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/gstreamer/gstlibcameraprovider.cpp
@@ -33,7 +33,6 @@ GST_DEBUG_CATEGORY_STATIC(provider_debug);
 
 enum {
 	PROP_DEVICE_NAME = 1,
-	PROP_AUTO_FOCUS_MODE = 2,
 };
 
 #define GST_TYPE_LIBCAMERA_DEVICE gst_libcamera_device_get_type()
@@ -43,7 +42,6 @@ G_DECLARE_FINAL_TYPE(GstLibcameraDevice, gst_libcamera_device,
 struct _GstLibcameraDevice {
 	GstDevice parent;
 	gchar *name;
-	controls::AfModeEnum auto_focus_mode = controls::AfModeManual;
 };
 
 G_DEFINE_TYPE(GstLibcameraDevice, gst_libcamera_device, GST_TYPE_DEVICE)
@@ -60,7 +58,6 @@ gst_libcamera_device_create_element(GstDevice *device, const gchar *name)
 	g_assert(source);
 
 	g_object_set(source, "camera-name", GST_LIBCAMERA_DEVICE(device)->name, nullptr);
-	g_object_set(source, "auto-focus-mode", GST_LIBCAMERA_DEVICE(device)->auto_focus_mode, nullptr);
 
 	return source;
 }
@@ -86,9 +83,6 @@ gst_libcamera_device_set_property(GObject *object, guint prop_id,
 	switch (prop_id) {
 	case PROP_DEVICE_NAME:
 		device->name = g_value_dup_string(value);
-		break;
-	case PROP_AUTO_FOCUS_MODE:
-		device->auto_focus_mode = static_cast<controls::AfModeEnum>(g_value_get_enum(value));
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
@@ -129,15 +123,6 @@ gst_libcamera_device_class_init(GstLibcameraDeviceClass *klass)
 						(GParamFlags)(G_PARAM_STATIC_STRINGS | G_PARAM_WRITABLE |
 							      G_PARAM_CONSTRUCT_ONLY));
 	g_object_class_install_property(object_class, PROP_DEVICE_NAME, pspec);
-
-	pspec = g_param_spec_enum("auto-focus-mode",
-				  "Set auto-focus mode",
-				  "Available options: AfModeManual, "
-				  "AfModeAuto or AfModeContinuous.",
-				  gst_libcamera_auto_focus_get_type(),
-				  static_cast<gint>(controls::AfModeManual),
-				  G_PARAM_WRITABLE);
-	g_object_class_install_property(object_class, PROP_AUTO_FOCUS_MODE, pspec);
 }
 
 static GstDevice *