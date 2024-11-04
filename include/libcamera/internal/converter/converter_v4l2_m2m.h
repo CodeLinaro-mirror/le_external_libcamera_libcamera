405779ebe3a6144a379f62d1622792d960242517
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2024-11-04 23:49:14 +0100

    libcamera: converter: Add functions to adjust config
    Add to the Converter interface two functions used by pipeline handlers
    to validate and adjust the converter input and output configurations
    by specifying the desired alignment for the adjustment.
    
    Add the adjustInputSize() and adjustOutputSize() functions that allows
    to adjust the converter input/output sizes with the desired alignment.
    
    Add a validateOutput() function meant to be used by the pipeline
    handler implementations of validate(). The function adjusts a
    StreamConfiguration to a valid configuration produced by the Converter.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    
    ---
    Changes in v3:
    - Added this patch

--- include/libcamera/internal/converter/converter_v4l2_m2m.h
+++ include/libcamera/internal/converter/converter_v4l2_m2m.h
@@ -47,6 +47,11 @@ public:
 	std::tuple<unsigned int, unsigned int>
 	strideAndFrameSize(const PixelFormat &pixelFormat, const Size &size);
 
+	Size adjustInputSize(const PixelFormat &pixFmt,
+			     const Size &size, Alignments align = Alignment::Down) override;
+	Size adjustOutputSize(const PixelFormat &pixFmt,
+			      const Size &size, Alignments align = Alignment::Down) override;
+
 	int configure(const StreamConfiguration &inputCfg,
 		      const std::vector<std::reference_wrapper<StreamConfiguration>> &outputCfg);
 	int exportBuffers(const Stream *stream, unsigned int count,
@@ -54,6 +59,9 @@ public:
 
 	int start();
 	void stop();
+
+	int validateOutput(StreamConfiguration *cfg, bool *adjusted,
+			   Alignments align = Alignment::Down) override;
 
 	int queueBuffers(FrameBuffer *input,
 			 const std::map<const Stream *, FrameBuffer *> &outputs);
@@ -100,6 +108,9 @@ private:
 
 		std::pair<Rectangle, Rectangle> inputCropBounds_;
 	};
+
+	Size adjustSizes(const Size &size, const std::vector<SizeRange> &ranges,
+			 Alignments align);
 
 	std::unique_ptr<V4L2M2MDevice> m2m_;
 
