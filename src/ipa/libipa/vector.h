c1887574c1f312598bbc52c17c9f3f8cde4894c2
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2024-11-18 02:07:30 +0200

    ipa: libipa: vector: Rename the dot product operator*() to dot()
    The Vector class defines a set of arithmetic operators between two
    vectors or a vector and a scalar. All the operators perform element-wise
    operations, except for the operator*() that computes the dot product.
    This is inconsistent and confusing. Replace the operator with a dot()
    function.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/libipa/vector.h
+++ src/ipa/libipa/vector.h
@@ -90,14 +90,6 @@ public:
 		return ret;
 	}
 
-	constexpr T operator*(const Vector<T, Rows> &other) const
-	{
-		T ret = 0;
-		for (unsigned int i = 0; i < Rows; i++)
-			ret += data_[i] * other[i];
-		return ret;
-	}
-
 	constexpr Vector<T, Rows> operator*(T factor) const
 	{
 		Vector<T, Rows> ret;
@@ -111,6 +103,14 @@ public:
 		Vector<T, Rows> ret;
 		for (unsigned int i = 0; i < Rows; i++)
 			ret[i] = data_[i] / factor;
+		return ret;
+	}
+
+	constexpr T dot(const Vector<T, Rows> &other) const
+	{
+		T ret = 0;
+		for (unsigned int i = 0; i < Rows; i++)
+			ret += data_[i] * other[i];
 		return ret;
 	}
 
