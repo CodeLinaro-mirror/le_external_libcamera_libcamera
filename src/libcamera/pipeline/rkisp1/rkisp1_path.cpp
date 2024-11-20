7e38cab25d598d5cd4cbb2cd54363361cfce7778
Stefan Klug <stefan.klug@ideasonboard.com>
2024-11-20 09:57:41 +0100

    libcamera: rkisp1: Keep aspect ratio on imx8mp
    In the current code, the input stage of the image resizer is used to
    apply a crop to keep the aspect ratio in cases where the requested
    output aspect ratio differs from the one of the selected sensor mode. On
    the imx8mp the resizer hardware is not capable of cropping (for
    reference see also rkisp1-resizer.c:rkisp1_rsz_set_sink_crop() in the
    linux kernel v6.10).
    
    Therefore apply the necessary cropping on the output of the ISP (on the
    image stabilization block). The cropping code on the image resizer
    doesn't need modifications as the requested crop gets ignored by the
    kernel.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/rkisp1/rkisp1_path.cpp
+++ src/libcamera/pipeline/rkisp1/rkisp1_path.cpp
@@ -417,6 +417,12 @@ int RkISP1Path::configure(const StreamConfiguration &config,
 	/*
 	 * Crop on the resizer input to maintain FOV before downscaling.
 	 *
+	 * Note that this does not apply on the imx8mp, where the cropping needs
+	 * to be done on the ImageStabilizer output and therefore is configured
+	 * before this stage. For simplicity we still set the crop. This gets
+	 * ignored by the kernel driver because the hardware is missing the
+	 * capability.
+	 *
 	 * \todo The alignment to a multiple of 2 pixels is required but may
 	 * change the aspect ratio very slightly. A more advanced algorithm to
 	 * compute the resizer input crop rectangle is needed, and it should
