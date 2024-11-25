ec4f68c99c65a6d4c37699c0d492d2d98d9a22f5
Stefan Klug <stefan.klug@ideasonboard.com>
2024-11-25 16:14:17 +0100

    pipeline: rkisp1: Add ScalerMaximumCrop property
    The ScalerMaximumCrop property holds the biggest allowed ScalerCrop
    value. Add it to the rkisp1.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/rkisp1/rkisp1.cpp
+++ src/libcamera/pipeline/rkisp1/rkisp1.cpp
@@ -24,6 +24,7 @@
 #include <libcamera/control_ids.h>
 #include <libcamera/formats.h>
 #include <libcamera/framebuffer.h>
+#include <libcamera/property_ids.h>
 #include <libcamera/request.h>
 #include <libcamera/stream.h>
 #include <libcamera/transform.h>
@@ -1247,6 +1248,7 @@ int PipelineHandlerRkISP1::updateControls(RkISP1CameraData *data)
 								     dewarperSensorCrop_);
 
 		controls[&controls::ScalerCrop] = ControlInfo(min, max, max);
+		data->properties_.set(properties::ScalerCropMaximum, max);
 		activeCrop_ = max;
 	}
 
