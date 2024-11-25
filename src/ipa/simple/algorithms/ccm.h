2fba544013118ac627c1505371a95fa5c24dc639
Milan Zamazal <mzamazal@redhat.com>
2024-11-25 19:03:07 +0100

    libcamera: software_isp: Add CCM algorithm
    This patch adds color correction matrix (CCM) algorithm to software ISP.
    It is based on the corresponding algorithm in rkisp1.
    
    The primary difference against hardware pipelines is that applying the
    CCM is optional.  Applying CCM causes a significant slowdown, time
    needed to process a frame raises by 40-90% on tested platforms.  If CCM
    is really needed, it can be applied, if not, it's better to stick
    without it.  This can be configured by presence or omission of Ccm
    algorithm in the tuning file.
    
    CCM is changed only if the determined temperature changes by at least
    100 K (an arbitrarily selected value), to avoid recomputing the matrices
    and lookup tables all the time.  Some ugly unsigned->signed int
    typecasting is needed to compare the temperatures.
    
    The outputs of the algorithm are not used yet, they will be enabled in
    followup patches.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/ipa/simple/algorithms/ccm.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2024, Red Hat Inc.
+ *
+ * Color correction matrix
+ */
+
+#pragma once
+
+#include "libipa/interpolator.h"
+#include "libipa/matrix.h"
+
+#include "algorithm.h"
+
+namespace libcamera {
+
+namespace ipa::soft::algorithms {
+
+class Ccm : public Algorithm
+{
+public:
+	Ccm() = default;
+	~Ccm() = default;
+
+	int init(IPAContext &context, const YamlObject &tuningData) override;
+	void prepare(IPAContext &context,
+		     const uint32_t frame,
+		     IPAFrameContext &frameContext,
+		     DebayerParams *params) override;
+	void process(IPAContext &context, const uint32_t frame,
+		     IPAFrameContext &frameContext,
+		     const SwIspStats *stats,
+		     ControlList &metadata) override;
+
+private:
+	static unsigned int kTemperatureThreshold;
+	unsigned int ct_;
+	bool ccmEnabled_;
+	Interpolator<Matrix<float, 3, 3>> ccm_;
+};
+
+} /* namespace ipa::soft::algorithms */
+
+} /* namespace libcamera */
