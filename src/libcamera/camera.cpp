raspberrypi/libcamera

commit 4217c9f1aa863cf629bb2420be352b0c39988a5b

libcamera: camera: Zero streams before validate()
The current implementation of the Camera::configure() method zeroes the
stream pointers assigned to the StreamConfiguration items before calling
the pipeline handler configure() operation, just after the
CameraConfiguration has been validated.

This discards the stream assignment performed at pipeline hander
validation time, requiring platforms that need to perform that early
assignment to maintain the association in place with custom data
structures.

To allow pipeline handlers to use StreamConfiguration::setStream() at
validate() time, zero the stream assignment before calling validate().

Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/camera.cpp
@@ -753,6 +753,9 @@ int Camera::configure(CameraConfiguration *config)
 	if (ret < 0)
 		return ret;
 
+	for (auto it : *config)
+		it.setStream(nullptr);
+
 	if (config->validate() != CameraConfiguration::Valid) {
 		LOG(Camera, Error)
 			<< "Can't configure camera with invalid configuration";
@@ -763,7 +766,6 @@ int Camera::configure(CameraConfiguration *config)
 
 	for (unsigned int index = 0; index < config->size(); ++index) {
 		StreamConfiguration &cfg = config->at(index);
-		cfg.setStream(nullptr);
 		msg << " (" << index << ") " << cfg.toString();
 	}
 

CodeLinaro-mirror/le_external_libcamera_libcamera

commit 8d0e7531a0f1de74e50378d4ffd49856797e1203

libcamera: camera: Fix clearing of stream associations before `validate()`
A copy is made in the range-based for loop, and thus `setStream()`
operates on this copy, leading to the `stream_` member of the given
`StreamConfiguration` object in `*config` never being set to `nullptr`.

Fix that by taking a reference in the range-based for loop.

Also rename the variable from `it` since it is not an iterator.

Fixes: 4217c9f1aa863c ("libcamera: camera: Zero streams before validate()")
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/camera.cpp
@@ -1178,8 +1178,8 @@ int Camera::configure(CameraConfiguration *config)
 	if (ret < 0)
 		return ret;
 
-	for (auto it : *config)
-		it.setStream(nullptr);
+	for (auto &cfg : *config)
+		cfg.setStream(nullptr);
 
 	if (config->validate() != CameraConfiguration::Valid) {
 		LOG(Camera, Error)