21d6a3f34a6fd23cdcf5061d5431ad95ac57ae3f
Daniel Scally <dan.scally@ideasonboard.com>
2024-11-27 13:32:32 +0000

    libcamera: camera_sensor_properties: Add ov7251 and ov9281
    Add entries for the OmniVision OV7251 and OV9281 sensors. The control
    delay values are drawn from the Raspberry Pi CameraSensorHelper class
    for these sensors - the pixel sizes from the datasheets. Both sensors
    are monochrome and do not have test pattern settings that correspond
    to defined control values.
    
    Signed-off-by: Daniel Scally <dan.scally@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/sensor/camera_sensor_properties.cpp
+++ src/libcamera/sensor/camera_sensor_properties.cpp
@@ -360,6 +360,16 @@ const CameraSensorProperties *CameraSensorProperties::get(const std::string &sen
 				.hblankDelay = 2
 			},
 		} },
+		{ "ov7251", {
+			.unitCellSize = { 3000, 3000 },
+			.testPatternModes =  { },
+			.sensorDelays = {
+				.exposureDelay = 2,
+				.gainDelay = 2,
+				.vblankDelay = 2,
+				.hblankDelay = 2
+			},
+		} },
 		{ "ov8858", {
 			.unitCellSize = { 1120, 1120 },
 			.testPatternModes = {
@@ -388,6 +398,16 @@ const CameraSensorProperties *CameraSensorProperties::get(const std::string &sen
 				 */
 			},
 			.sensorDelays = { },
+		} },
+		{ "ov9281", {
+			.unitCellSize = { 3000, 3000 },
+			.testPatternModes =  { },
+			.sensorDelays = {
+				.exposureDelay = 2,
+				.gainDelay = 2,
+				.vblankDelay = 2,
+				.hblankDelay = 2
+			},
 		} },
 		{ "ov13858", {
 			.unitCellSize = { 1120, 1120 },
