340a4a292a8ef278caa9225dc45c7afee09d2fe1
Stefan Klug <stefan.klug@ideasonboard.com>
2024-12-06 15:52:22 +0100

    ipa: rkisp1: awb: Load white balance gains from tuning file
    For the implementation of a manual colour temperature setting, it is
    necessary to read predefined colour gains per colour temperature from
    the tuning file. Implement this in a backwards compatible way. If no
    gains are contained in the tuning file, loading just continues as
    before.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/rkisp1/algorithms/awb.cpp
+++ src/ipa/rkisp1/algorithms/awb.cpp
@@ -42,6 +42,24 @@ Awb::Awb()
 }
 
 /**
+ * \copydoc libcamera::ipa::Algorithm::init
+ */
+int Awb::init([[maybe_unused]] IPAContext &context, const YamlObject &tuningData)
+{
+	Interpolator<Vector<double, 2>> gains;
+	int ret = gains.readYaml(tuningData["gains"], "ct", "gains");
+	if (ret < 0)
+		LOG(RkISP1Awb, Warning)
+			<< "Failed to parse 'gains' "
+			<< "parameter from tuning file; "
+			<< "manual colour temperature will not work properly";
+	else
+		gains_ = gains;
+
+	return 0;
+}
+
+/**
  * \copydoc libcamera::ipa::Algorithm::configure
  */
 int Awb::configure(IPAContext &context,
