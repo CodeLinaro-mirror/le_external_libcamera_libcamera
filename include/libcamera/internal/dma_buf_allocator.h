raspberrypi/libcamera

commit 39482d59fe7160740ea5dd61f3ed965a88d848ce

DmaBufAllocator: Add Dma Buffer synchronization function & helper class
To synchronize CPU access with mmap and hardware access on DMA buffers,
using `DMA_BUF_IOCTL_SYNC` is required. This patch adds a function and
a helper class to allow users to sync buffers more easily.

Signed-off-by: Han-Lin Chen <hanlinchen@chromium.org>
Co-developed-by: Harvey Yang <chenghaoyang@chromium.org>
Signed-off-by: Harvey Yang <chenghaoyang@chromium.org>
Reviewed-by: Milan Zamazal <mzamazal@redhat.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

include/libcamera/internal/dma_buf_allocator.h
@@ -12,6 +12,7 @@
 #include <vector>
 
 #include <libcamera/base/flags.h>
+#include <libcamera/base/shared_fd.h>
 #include <libcamera/base/unique_fd.h>
 
 namespace libcamera {
@@ -46,6 +47,26 @@ private:
 	UniqueFD allocFromUDmaBuf(const char *name, std::size_t size);
 	UniqueFD providerHandle_;
 	DmaBufAllocatorFlag type_;
+};
+
+class DmaSyncer final
+{
+public:
+	enum class SyncType {
+		Read = 0,
+		Write,
+		ReadWrite,
+	};
+
+	explicit DmaSyncer(SharedFD fd, SyncType type = SyncType::ReadWrite);
+
+	~DmaSyncer();
+
+private:
+	void sync(uint64_t step);
+
+	SharedFD fd_;
+	uint64_t flags_ = 0;
 };
 
 LIBCAMERA_FLAGS_ENABLE_OPERATORS(DmaBufAllocator::DmaBufAllocatorFlag)

src/libcamera/dma_buf_allocator.cpp
@@ -262,4 +262,78 @@ DmaBufAllocator::createBuffer(std::string name,
 	return std::make_unique<FrameBuffer>(planes);
 }
 
+/**
+ * \class DmaSyncer
+ * \brief Helper class for dma-buf's synchronization
+ *
+ * This class wraps a userspace dma-buf's synchronization process with an
+ * object's lifetime.
+ *
+ * It's used when the user needs to access a dma-buf with CPU, mostly mapped
+ * with MappedFrameBuffer, so that the buffer is synchronized between CPU and
+ * ISP.
+ */
+
+/**
+ * \enum DmaSyncer::SyncType
+ * \brief Read and/or write access via the CPU map
+ * \var DmaSyncer::Read
+ * \brief Indicates that the mapped dma-buf will be read by the client via the
+ * CPU map
+ * \var DmaSyncer::Write
+ * \brief Indicates that the mapped dm-buf will be written by the client via the
+ * CPU map
+ * \var DmaSyncer::ReadWrite
+ * \brief Indicates that the mapped dma-buf will be read and written by the
+ * client via the CPU map
+ */
+
+/**
+ * \brief Construct a DmaSyncer with a dma-buf's fd and the access type
+ * \param[in] fd The dma-buf's file descriptor to synchronize
+ * \param[in] type Read and/or write access via the CPU map
+ */
+DmaSyncer::DmaSyncer(SharedFD fd, SyncType type)
+	: fd_(fd)
+{
+	switch (type) {
+	case SyncType::Read:
+		flags_ = DMA_BUF_SYNC_READ;
+		break;
+	case SyncType::Write:
+		flags_ = DMA_BUF_SYNC_WRITE;
+		break;
+	case SyncType::ReadWrite:
+		flags_ = DMA_BUF_SYNC_RW;
+		break;
+	}
+
+	sync(DMA_BUF_SYNC_START);
+}
+
+DmaSyncer::~DmaSyncer()
+{
+	sync(DMA_BUF_SYNC_END);
+}
+
+void DmaSyncer::sync(uint64_t step)
+{
+	struct dma_buf_sync sync = {
+		.flags = flags_ | step
+	};
+
+	int ret;
+	do {
+		ret = ioctl(fd_.get(), DMA_BUF_IOCTL_SYNC, &sync);
+	} while (ret && (errno == EINTR || errno == EAGAIN));
+
+	if (ret) {
+		ret = errno;
+		LOG(DmaBufAllocator, Error)
+			<< "Unable to sync dma fd: " << fd_.get()
+			<< ", err: " << strerror(ret)
+			<< ", flags: " << sync.flags;
+	}
+}
+
 } /* namespace libcamera */

CodeLinaro-mirror/le_external_libcamera_libcamera

commit e8ff9fee7c52ae399990329ce7e02f38ec378871

DmaBufAllocator: Make DmaSyncer non-copyable
As DmaSyncer does sync start/end in the c'tor/d'tor, copying a DmaSyncer
instance would trigger sync end earlier than expected. This patch makes
it non-copyable to avoid the issue.

Fixes: 39482d59fe71 ("DmaBufAllocator: Add Dma Buffer synchronization function & helper class")
Signed-off-by: Harvey Yang <chenghaoyang@chromium.org>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

include/libcamera/internal/dma_buf_allocator.h
@@ -60,9 +60,14 @@ public:
 
 	explicit DmaSyncer(SharedFD fd, SyncType type = SyncType::ReadWrite);
 
+	DmaSyncer(DmaSyncer &&) = default;
+	DmaSyncer &operator=(DmaSyncer &&) = default;
+
 	~DmaSyncer();
 
 private:
+	LIBCAMERA_DISABLE_COPY(DmaSyncer)
+
 	void sync(uint64_t step);
 
 	SharedFD fd_;

src/libcamera/dma_buf_allocator.cpp
@@ -311,6 +311,16 @@ DmaSyncer::DmaSyncer(SharedFD fd, SyncType type)
 	sync(DMA_BUF_SYNC_START);
 }
 
+/**
+ * \fn DmaSyncer::DmaSyncer(DmaSyncer &&) = default;
+ * \brief Enable move on class DmaSyncer
+ */
+
+/**
+ * \fn DmaSyncer::operator=(DmaSyncer &&) = default;
+ * \brief Enable move on class DmaSyncer
+ */
+
 DmaSyncer::~DmaSyncer()
 {
 	sync(DMA_BUF_SYNC_END);