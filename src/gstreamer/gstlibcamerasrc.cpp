f5963ecfd2fc7b032bab52fe2f394e7839cb459e
Hou Qi <qi.hou@nxp.com>
2024-12-13 15:03:19 +0900

    gstreamer: keep same transfer with that in negotiated caps
    The conversions back and forth between GStreamer colorimetry and
    libcamera color space are not invariant for the bt601 colorimetry.
    The reason is that Rec709 transfer function defined in GStreamer
    as GST_VIDEO_TRANSFER_BT709 (5), is to be replaced by its alias
    GST_VIDEO_TRANSFER_BT601 (16) only for the case of bt601 (aka 2:4:16:4)
    colorimetry - see [1].
    
    Currently the composition of the GStreamer/libcamera conversions:
    colorimetry_from_colorspace (colorspace_from_colorimetry (bt601))
    returns 2:4:5:4 instead of the expected 2:4:16:4 (bt601). This
    causes negotiation error when the downstream element explicitly
    expects bt601 colorimetry.
    
    Minimal example to reproduce the issue is with a pipeline handler
    that do not set the optional color space in the stream configuration,
    for instance vimc or imx8-isi:
    export LIBCAMERA_PIPELINES_MATCH_LIST="vimc,imx8-isi"
    gst-launch-1.0 -v libcamerasrc ! video/x-raw,colorimetry=bt601 ! fakesink
    
    Above pipeline fails to start. This change memorizes downstream required
    transfer function when mapped libcamera transfer is Rec709 in
    gst_libcamera_configure_stream_from_caps(), and restores the transfer
    function in gst_libcamera_stream_formats_to_caps().
    
    [1] https://gitlab.freedesktop.org/gstreamer/gst-plugins-base/-/merge_requests/724
    
    Bug: https://bugs.libcamera.org/show_bug.cgi?id=150
    Signed-off-by: Hou Qi <qi.hou@nxp.com>
    Reviewed-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/gstreamer/gstlibcamerasrc.cpp
+++ src/gstreamer/gstlibcamerasrc.cpp
@@ -433,12 +433,14 @@ static bool
 gst_libcamera_src_negotiate(GstLibcameraSrc *self)
 {
 	GstLibcameraSrcState *state = self->state;
+	GstVideoTransferFunction transfer[state->srcpads_.size()];
 
 	g_autoptr(GstStructure) element_caps = gst_structure_new_empty("caps");
 
 	for (gsize i = 0; i < state->srcpads_.size(); i++) {
 		GstPad *srcpad = state->srcpads_[i];
 		StreamConfiguration &stream_cfg = state->config_->at(i);
+		transfer[i] = GST_VIDEO_TRANSFER_UNKNOWN;
 
 		/* Retrieve the supported caps. */
 		g_autoptr(GstCaps) filter = gst_libcamera_stream_formats_to_caps(stream_cfg.formats());
@@ -448,7 +450,7 @@ gst_libcamera_src_negotiate(GstLibcameraSrc *self)
 
 		/* Fixate caps and configure the stream. */
 		caps = gst_caps_make_writable(caps);
-		gst_libcamera_configure_stream_from_caps(stream_cfg, caps);
+		gst_libcamera_configure_stream_from_caps(stream_cfg, caps, &transfer[i]);
 		gst_libcamera_get_framerate_from_caps(caps, element_caps);
 	}
 
@@ -476,7 +478,7 @@ gst_libcamera_src_negotiate(GstLibcameraSrc *self)
 		GstPad *srcpad = state->srcpads_[i];
 		const StreamConfiguration &stream_cfg = state->config_->at(i);
 
-		g_autoptr(GstCaps) caps = gst_libcamera_stream_configuration_to_caps(stream_cfg);
+		g_autoptr(GstCaps) caps = gst_libcamera_stream_configuration_to_caps(stream_cfg, transfer[i]);
 		gst_libcamera_framerate_to_caps(caps, element_caps);
 
 		if (!gst_pad_push_event(srcpad, gst_event_new_caps(caps)))
