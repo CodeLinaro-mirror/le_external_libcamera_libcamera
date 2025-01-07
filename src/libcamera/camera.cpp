092f464120c1101ba512f974283944911b2f43aa
Paul Elder <paul.elder@ideasonboard.com>
2025-01-06 19:46:51 -0600

    libcamera: camera: Pre-process AeEnable control
    Handle the AeEnable under-the-hood in the Camera class, such that
    AeEnable activates ExposureTimeMode and AnalogueGain together. This
    allows applications the convenience of setting auto/manual mode of all
    of the AE-related controls, as well as protecting applications against a
    nasty behavior change if an aperture control is added in the future.
    This also moves common handling code out of the IPA.
    
    While we also want to inject AeEnable in Camera::controls() so that IPAs
    don't have to report it, it is technically difficult at the moment as
    ControlInfoMaps are not easily modifiable.
    
    Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
    
    ---
    New in v6

--- src/libcamera/camera.cpp
+++ src/libcamera/camera.cpp
@@ -19,6 +19,7 @@
 #include <libcamera/base/thread.h>
 
 #include <libcamera/color_space.h>
+#include <libcamera/control_ids.h>
 #include <libcamera/framebuffer_allocator.h>
 #include <libcamera/request.h>
 #include <libcamera/stream.h>
@@ -1322,6 +1323,23 @@ int Camera::queueRequest(Request *request)
 		if (d->activeStreams_.find(stream) == d->activeStreams_.end()) {
 			LOG(Camera, Error) << "Invalid request";
 			return -EINVAL;
+		}
+	}
+
+	/* Pre-process AeEnable */
+	ControlList &controls = request->controls();
+	const auto &aeEnable = controls.get(controls::AeEnable);
+	if (aeEnable) {
+		if (!controls.contains(controls::AnalogueGainMode.id())) {
+			controls.set(controls::AnalogueGainMode,
+				     *aeEnable ? controls::AnalogueGainModeAuto
+					       : controls::AnalogueGainModeManual);
+		}
+
+		if (!controls.contains(controls::ExposureTimeMode.id())) {
+			controls.set(controls::ExposureTimeMode,
+				     *aeEnable ? controls::ExposureTimeModeAuto
+					       : controls::ExposureTimeModeManual);
 		}
 	}
 
