818fb83f5969df30f602a8cc2a4fdec1619de155
Milan Zamazal <mzamazal@redhat.com>
2025-01-13 14:34:05 +0100

    ipa: simple: Report exposure in metadata
    Report the exposure+gain in metadata.
    
    The exposure value is especially dubious because it should be in
    microseconds but it's handled using V4L2_CID_EXPOSURE control, which
    doesn't specify the unit, see
    https://www.kernel.org/doc/html/latest/userspace-api/media/v4l/control.html.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/simple/algorithms/agc.cpp
+++ src/ipa/simple/algorithms/agc.cpp
@@ -11,6 +11,8 @@
 
 #include <libcamera/base/log.h>
 
+#include "control_ids.h"
+
 namespace libcamera {
 
 LOG_DEFINE_CATEGORY(IPASoftExposure)
@@ -99,8 +101,11 @@ void Agc::process(IPAContext &context,
 		  [[maybe_unused]] const uint32_t frame,
 		  [[maybe_unused]] IPAFrameContext &frameContext,
 		  const SwIspStats *stats,
-		  [[maybe_unused]] ControlList &metadata)
+		  ControlList &metadata)
 {
+	metadata.set(controls::ExposureTime, frameContext.sensor.exposure);
+	metadata.set(controls::AnalogueGain, frameContext.sensor.gain);
+
 	/*
 	 * Calculate Mean Sample Value (MSV) according to formula from:
 	 * https://www.araa.asn.au/acra/acra2007/papers/paper84final.pdf
