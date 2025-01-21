b17d26ed3d17e744a5658249e490c2846d581d9a
Barnabás Pőcze <pobrn@protonmail.com>
2025-01-21 18:56:11 +0000

    libcamera: base: log: Pass dynamic prefix through
    Use move construction to essentially pass through the string
    returned by `Loggable::logPrefix()` to avoid an unnecessary copy.
    
    Signed-off-by: Barnabás Pőcze <pobrn@protonmail.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- include/libcamera/base/log.h
+++ include/libcamera/base/log.h
@@ -64,7 +64,7 @@ class LogMessage
 public:
 	LogMessage(const char *fileName, unsigned int line,
 		   const LogCategory &category, LogSeverity severity,
-		   const std::string &prefix = std::string());
+		   std::string prefix = {});
 
 	LogMessage(LogMessage &&);
 	~LogMessage();
