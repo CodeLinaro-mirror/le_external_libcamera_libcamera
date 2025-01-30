63c6f60ccaad3e61bd77954f26b2df10cc220424
Milan Zamazal <mzamazal@redhat.com>
2025-01-30 19:14:46 +0100

    libcamera: software_isp: Track whether CCM is enabled
    Applying color correction matrix (CCM) in software ISP is optional due
    to performance reasons.  CCM is applied if and only if `Ccm' algorithm
    is present in the tuning file.
    
    Software ISP debayering is a performance critical piece of code and we
    do not want to use dynamic conditionals there.  Therefore we pass
    information about CCM application to debayering configuration and let it
    select the right versions of debayering functions using templates.  This
    is a trick similar to the previously used one for adding or not adding
    an alpha channel to the output.
    
    Debayering gets this information but it ignores it in this patch.
    Actual processing with CCM is added in the followup patch.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- include/libcamera/internal/software_isp/software_isp.h
+++ include/libcamera/internal/software_isp/software_isp.h
@@ -97,6 +97,7 @@ private:
 	SharedMemObject<DebayerParams> sharedParams_;
 	DebayerParams debayerParams_;
 	DmaBufAllocator dmaHeap_;
+	bool ccmEnabled_;
 
 	std::unique_ptr<ipa::soft::IPAProxySoft> ipa_;
 };
