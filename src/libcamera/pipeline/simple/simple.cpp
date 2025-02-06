8035a09984e7dd25548be4787c3bd3ff35e6d924
Milan Zamazal <mzamazal@redhat.com>
2025-02-06 17:16:01 +0100

    config: Allow enabling software ISP in runtime
    This patch allows enabling or disabling software ISP in runtime in
    addition to compile time.  This can be useful for software ISP testing
    on various platforms as well as for overriding the defaults in case the
    defaults don't work well (e.g. hardware ISP may or may not work on
    i.MX8MP depending on the kernel and libcamera patches present in the
    given system).
    
    The configuration is specified as follows:
    
      configuration:
        pipelines:
          simple:
            supported_devices:
            - driver: DRIVER-NAME
              software_isp: BOOLEAN
            - ...
    
    For example:
    
      configuration:
        pipelines:
          simple:
            supported_devices:
            - driver: mxc-isi
              software_isp: true
    
    The overall configuration of enabling or disabling software ISP may get
    dropped in future but this patch is still useful in the meantime.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/simple/simple.cpp
+++ src/libcamera/pipeline/simple/simple.cpp
@@ -35,6 +35,7 @@
 #include "libcamera/internal/converter.h"
 #include "libcamera/internal/delayed_controls.h"
 #include "libcamera/internal/device_enumerator.h"
+#include "libcamera/internal/global_configuration.h"
 #include "libcamera/internal/media_device.h"
 #include "libcamera/internal/pipeline_handler.h"
 #include "libcamera/internal/software_isp/software_isp.h"
@@ -1560,6 +1561,17 @@ bool SimplePipelineHandler::match(DeviceEnumerator *enumerator)
 	}
 
 	swIspEnabled_ = info->swIspEnabled;
+	for (GlobalConfiguration::Configuration entry :
+	     GlobalConfiguration::configuration()
+		     ["pipelines"]["simple"]["supported_devices"]
+			     .asList()) {
+		auto name = entry["driver"].get<std::string>();
+		if (name == info->driver) {
+			swIspEnabled_ = entry["software_isp"].get<bool>().value_or(swIspEnabled_);
+			LOG(SimplePipeline, Debug) << "Overriding software ISP to " << swIspEnabled_;
+			break;
+		}
+	}
 
 	/* Locate the sensors. */
 	std::vector<MediaEntity *> sensors = locateSensors(media);
