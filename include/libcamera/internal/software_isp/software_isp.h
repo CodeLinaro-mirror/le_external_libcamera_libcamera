f81f511f1b0cdacb2404bcd3c405e67ac591953c
Milan Zamazal <mzamazal@redhat.com>
2025-02-24 13:08:52 +0100

    libcamera: software_isp: Handle queued input buffers on stop
    When SoftwareIsp stops, input and output buffers queued to it may not
    yet be fully processed.  They will be eventually returned but stop means
    stop, there should be no processing related actions invoked afterwards.
    
    Let's stop forwarding processed input buffers from SoftwareIsp slots
    when SoftwareIsp is stopped.  Let's track the queued input buffers and
    return them back for capture in SoftwareIsp::stop().
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- include/libcamera/internal/software_isp/software_isp.h
+++ include/libcamera/internal/software_isp/software_isp.h
@@ -102,6 +102,7 @@ private:
 	std::unique_ptr<ipa::soft::IPAProxySoft> ipa_;
 	bool running_;
 	std::deque<FrameBuffer *> queuedOutputBuffers_;
+	std::deque<FrameBuffer *> queuedInputBuffers_;
 };
 
 } /* namespace libcamera */
