c1d98d7fe2703fe20af1cec85dfeee94e57ec464
Stanislaw Gruszka <stanislaw.gruszka@linux.intel.com>
2025-02-25 17:41:12 +0100

    libcamera: v4l2_device: add frame start event helpers
    Add helper to check if frame start event are supported by subdevice.
    Since kernel does not have interface to query supported events
    use subscribe interface.
    
    Additionally add helper for checking if the event enabled.
    Will be needed later to proper code flow if enabling the event
    fail for some reason.
    
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com> # v3
    Signed-off-by: Stanislaw Gruszka <stanislaw.gruszka@linux.intel.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/v4l2_device.cpp
+++ src/libcamera/v4l2_device.cpp
@@ -450,6 +450,25 @@ std::string V4L2Device::devicePath() const
 }
 
 /**
+ * \brief Check if frame start event is supported
+ * \return True if frame start event is supported, false otherwise
+ */
+
+bool V4L2Device::supportsFrameStartEvent()
+{
+	struct v4l2_event_subscription event {
+	};
+	event.type = V4L2_EVENT_FRAME_SYNC;
+
+	int ret = ioctl(VIDIOC_SUBSCRIBE_EVENT, &event);
+	if (ret)
+		return false;
+
+	ioctl(VIDIOC_UNSUBSCRIBE_EVENT, &event);
+	return true;
+}
+
+/**
  * \brief Enable or disable frame start event notification
  * \param[in] enable True to enable frame start events, false to disable them
  *
