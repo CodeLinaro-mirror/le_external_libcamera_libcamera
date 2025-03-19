17612e6866ff46ec9aee8fd4ea1cea21c404f5c6
Stefan Klug <stefan.klug@ideasonboard.com>
2025-03-19 17:11:21 +0100

    ipa: rkisp1: awb: Take the CCM into account for the AWB gains calculation
    The AWB measurements are taken after the CCM. This can be seen by
    enabling debug logging on AWB, disabling AWB (stats will still be
    processed) and manually chaning the CCM.
    
    This means that the estimated colour temperature and the corresponding
    CCM also lead to changed rgbMeans which in turn leads to oscillations.
    Fix that by applying the inverse transform on the rgbMeans.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/rkisp1/algorithms/awb.cpp
+++ src/ipa/rkisp1/algorithms/awb.cpp
@@ -420,6 +420,12 @@ RGB<double> Awb::calculateRgbMeans(const IPAFrameContext &frameContext, const rk
 	}
 
 	/*
+	 * The ISP computes the AWB means after applying the ccm. Apply the
+	 * inverse as we want to get the raw means before the colour gains.
+	 */
+	rgbMeans = frameContext.ccm.ccm.inverse() * rgbMeans;
+
+	/*
 	 * The ISP computes the AWB means after applying the colour gains,
 	 * divide by the gains that were used to get the raw means from the
 	 * sensor.
