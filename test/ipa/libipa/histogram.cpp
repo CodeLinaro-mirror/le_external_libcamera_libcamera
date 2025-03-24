fd08c098d372b844a0fcc101d5af86f714f756d7
Stefan Klug <stefan.klug@ideasonboard.com>
2025-03-24 18:07:40 +0100

    test: ipa: libipa: histogram: Add tests for small inter quantile mean ranges
    Add tests for small inter quantile mean ranges. These cases were
    fixed in the last commit.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- test/ipa/libipa/histogram.cpp
+++ test/ipa/libipa/histogram.cpp
@@ -55,6 +55,10 @@ protected:
 		/* Test quantile means that are in the middle of the bins */
 		ASSERT_EQ(hist.interQuantileMean(0.25, 0.75), 1.0);
 
+		/* Test small ranges at the borders of the histogram */
+		ASSERT_EQ(hist.interQuantileMean(0.0, 0.1), 0.1);
+		ASSERT_EQ(hist.interQuantileMean(0.9, 1.0), 1.9);
+
 		return TestPass;
 	}
 };
