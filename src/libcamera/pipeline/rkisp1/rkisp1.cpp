a9d928c9bd5126a5bec216c022b5708ea07e5455
Stefan Klug <stefan.klug@ideasonboard.com>
2025-03-31 16:43:45 +0200

    pipeline: rkisp1: Add more info when parameter queue fails
    When the rkisp1 driver of the running kernel supports less parameter
    types than the libcamera build and such a parameter is used, queuing of
    the parameter buffer fails with -EINVAL. For the time being we have no
    way to query the kernel for supported parameter types at runtime. So at
    least print a error message that points the user in that direction.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/rkisp1/rkisp1.cpp
+++ src/libcamera/pipeline/rkisp1/rkisp1.cpp
@@ -411,7 +411,15 @@ void RkISP1CameraData::paramsComputed(unsigned int frame, unsigned int bytesused
 		return;
 
 	info->paramBuffer->_d()->metadata().planes()[0].bytesused = bytesused;
-	pipe->param_->queueBuffer(info->paramBuffer);
+
+	int ret = pipe->param_->queueBuffer(info->paramBuffer);
+	if (ret < 0) {
+		LOG(RkISP1, Error) << "Failed to queue parameter buffer. "
+				   << "Maybe the kernel doesn't support the"
+				   << " required parameter types.";
+		return;
+	}
+
 	pipe->stat_->queueBuffer(info->statBuffer);
 
 	if (info->mainPathBuffer)
