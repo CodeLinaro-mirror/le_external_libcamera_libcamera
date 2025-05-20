raspberrypi/libcamera

commit 7e5f1e1cedf5bb299cede5418fab5307c3e65441

apps: cam: Add option to set stream orientation
Add a '--orientation|-o' option to the cam test application to set
an orientation to the image stream.

Supported values are the ones obtained by applying flips to the camera
sensor:
- rot0: no rotation
- rot180: rotate 180 degrees
- flip: vertical flip
- mirror: horizontal flip

Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
Reviewed-by: David Plowman <david.plowman@raspberrypi.com>
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/apps/cam/camera_session.cpp
@@ -65,6 +65,24 @@ CameraSession::CameraSession(CameraManager *cm,
 		return;
 	}
 
+	if (options_.isSet(OptOrientation)) {
+		std::string orientOpt = options_[OptOrientation].toString();
+		static const std::map<std::string, libcamera::Orientation> orientations{
+			{ "rot0", libcamera::Orientation::Rotate0 },
+			{ "rot180", libcamera::Orientation::Rotate180 },
+			{ "mirror", libcamera::Orientation::Rotate0Mirror },
+			{ "flip", libcamera::Orientation::Rotate180Mirror },
+		};
+
+		auto orientation = orientations.find(orientOpt);
+		if (orientation == orientations.end()) {
+			std::cerr << "Invalid orientation " << orientOpt << std::endl;
+			return;
+		}
+
+		config->orientation = orientation->second;
+	}
+
 	/* Apply configuration if explicitly requested. */
 	if (StreamKeyValueParser::updateConfiguration(config.get(),
 						      options_[OptStream])) {

src/apps/cam/main.cpp
@@ -133,6 +133,11 @@ int CamApp::parseOptions(int argc, char *argv[])
 			 "Capture until interrupted by user or until <count> frames captured",
 			 "capture", ArgumentOptional, "count", false,
 			 OptCamera);
+
+	parser.addOption(OptOrientation, OptionString,
+			 "Desired image orientation (rot0, rot180, mirror, flip)",
+			 "orientation", ArgumentRequired, "orientation", false,
+			 OptCamera);
 #ifdef HAVE_KMS
 	parser.addOption(OptDisplay, OptionString,
 			 "Display viewfinder through DRM/KMS on specified connector",

src/apps/cam/main.h
@@ -17,6 +17,7 @@ enum {
 	OptList = 'l',
 	OptListProperties = 'p',
 	OptMonitor = 'm',
+	OptOrientation = 'o',
 	OptSDL = 'S',
 	OptStream = 's',
 	OptListControls = 256,

CodeLinaro-mirror/le_external_libcamera_libcamera

commit d60c8b4aede63ce8d2a7f0f9f5a9bef896d58070

apps: cam: Support full orientation options
The Orientation control was implemented in commit 7e5f1e1cedf5 "apps:
cam: Add option to set stream orientation" but did not fully add all of
the supported Orientation types.

The upcoming dewarp support on RkISP1 will provide full rotation
implementation options, including previously difficult '90' degree
rotations.

Extend the cam application to support setting any of the valid options.

The existing 'mirror' and 'flip' options are not consistent with the
rest of the option definitions, but maintain them as useful aliases to
their corresponding type.

Fixes: 7e5f1e1cedf5 ("apps: cam: Add option to set stream orientation")
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/apps/cam/camera_session.cpp
@@ -97,7 +97,16 @@ CameraSession::CameraSession(CameraManager *cm,
 		std::string orientOpt = options_[OptOrientation].toString();
 		static const std::map<std::string, libcamera::Orientation> orientations{
 			{ "rot0", libcamera::Orientation::Rotate0 },
+			{ "rot90", libcamera::Orientation::Rotate90 },
 			{ "rot180", libcamera::Orientation::Rotate180 },
+			{ "rot270", libcamera::Orientation::Rotate270 },
+
+			{ "rot0mirror", libcamera::Orientation::Rotate0Mirror },
+			{ "rot90mirror", libcamera::Orientation::Rotate90Mirror },
+			{ "rot180mirror", libcamera::Orientation::Rotate180Mirror },
+			{ "rot270mirror", libcamera::Orientation::Rotate270Mirror },
+
+			/* Helpful aliases */
 			{ "mirror", libcamera::Orientation::Rotate0Mirror },
 			{ "flip", libcamera::Orientation::Rotate180Mirror },
 		};

src/apps/cam/main.cpp
@@ -136,7 +136,10 @@ int CamApp::parseOptions(int argc, char *argv[])
 			 OptCamera);
 
 	parser.addOption(OptOrientation, OptionString,
-			 "Desired image orientation (rot0, rot180, mirror, flip)",
+			 "Desired image orientation. Supported values:\n"
+			 "- rot0, rot90, rot180, rot270,\n"
+			 "- rot0mirror, rot90mirror, rot180mirror, rot270mirror,\n"
+			 "- mirror (alias for rot0mirror), flip (alias for rot180mirror)",
 			 "orientation", ArgumentRequired, "orientation", false,
 			 OptCamera);
 #ifdef HAVE_KMS