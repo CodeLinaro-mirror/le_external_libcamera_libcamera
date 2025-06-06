d6c999132ff6afdb4aa62b4f20e92400d4ed4199
Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
2025-06-06 18:41:37 +0200

    libcamera: base: cxx20: Add `type_identity{, _t}`
    `type_identity_t` can be used to force non-deduced contexts
    in templates, such as:
    
      void f(T x, type_identity_t<T> y)
    
    when calling `f(1u, 2)`, without `type_identity_t`, the compiler
    could not unambiguously deduce `T` (unsigned int vs int). However,
    with `type_identity_t`, the type of the argument passed to `y`
    will not be used to deduce `T`, only the argument passed to `x`.
    
    See https://en.cppreference.com/w/cpp/types/type_identity
    
    Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- include/libcamera/base/details/cxx20.h
+++ include/libcamera/base/details/cxx20.h
@@ -9,4 +9,7 @@
 
 namespace libcamera::details::cxx20 {
 
+template<typename T> struct type_identity { using type = T; };
+template<typename T> using type_identity_t = typename type_identity<T>::type;
+
 } /* namespace libcamera::details::cxx20 */
