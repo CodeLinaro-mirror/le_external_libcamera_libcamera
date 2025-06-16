deceddb618b7a89f74ff5cbe98ea70190c2b8247
Milan Zamazal <mzamazal@redhat.com>
2025-06-16 10:47:30 +0200

    config: Make configuration file configurable
    To support easy switching of configurations, let's introduce
    LIBCAMERA_CONFIG_NAME environment variable, which denotes:
    
    - The path of the configuration file to load if it is an absolute path; or
    - the path of the configuration file to load, relative to the configuration
      directories.
    
    If such a configuration file doesn't exist, no custom configuration is
    loaded.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/base/global_configuration.cpp
+++ src/libcamera/base/global_configuration.cpp
@@ -75,8 +75,28 @@ bool GlobalConfiguration::loadFile(const std::filesystem::path &fileName)
 	return true;
 }
 
+const std::vector<std::filesystem::path>
+	globalConfigurationDirectories = {
+		std::filesystem::path(LIBCAMERA_SYSCONF_DIR),
+		std::filesystem::path("/etc/libcamera"),
+	};
+
 void GlobalConfiguration::load()
 {
+	const char *libcameraConfigName =
+		utils::secure_getenv("LIBCAMERA_CONFIG_NAME");
+	if (!libcameraConfigName)
+		libcameraConfigName = "";
+	std::filesystem::path configName(libcameraConfigName);
+
+	if (configName.is_absolute()) {
+		loadFile(configName);
+		return;
+	}
+
+	if (configName.empty())
+		configName = std::filesystem::path("configuration.yaml");
+
 	std::filesystem::path userConfigurationDirectory;
 	char *xdgConfigHome = utils::secure_getenv("XDG_CONFIG_HOME");
 	if (xdgConfigHome) {
@@ -90,13 +110,13 @@ void GlobalConfiguration::load()
 
 	if (!userConfigurationDirectory.empty()) {
 		std::filesystem::path user_configuration_file =
-			userConfigurationDirectory / "libcamera" / "configuration.yaml";
+			userConfigurationDirectory / "libcamera" / configName;
 		if (loadFile(user_configuration_file))
 			return;
 	}
 
-	for (const auto &path : globalConfigurationFiles)
-		if (loadFile(path))
+	for (const auto &path : globalConfigurationDirectories)
+		if (loadFile(path / configName))
 			return;
 }
 
