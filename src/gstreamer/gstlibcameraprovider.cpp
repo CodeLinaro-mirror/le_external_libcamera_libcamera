27b1209a9455a3b643cd5f84eb0215124c2778e2
Umang Jain <uajain@igalia.com>
2025-07-03 18:06:47 +0530

    libcamera: gstreamer: Report mounting-rotation of camera sensor
    Report mounting-rotation of the camera sensor by reading the
    property::Rotation property from libcamera. This is reported
    in device properties for libcamerasrc.
    
    Signed-off-by: Umang Jain <uajain@igalia.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/gstreamer/gstlibcameraprovider.cpp
+++ src/gstreamer/gstlibcameraprovider.cpp
@@ -12,6 +12,7 @@
 
 #include <libcamera/camera.h>
 #include <libcamera/camera_manager.h>
+#include <libcamera/property_ids.h>
 
 #include "gstlibcamerasrc.h"
 #include "gstlibcamera-utils.h"
@@ -144,12 +145,19 @@ gst_libcamera_device_new(const std::shared_ptr<Camera> &camera)
 			gst_caps_append(caps, sub_caps);
 	}
 
+	const int32_t rotation = camera->properties().get(properties::Rotation).value_or(0);
+	g_autoptr(GstStructure) props =
+		gst_structure_new("camera-properties",
+				  "api.libcamera.mounting-rotation", G_TYPE_INT, rotation,
+				  nullptr);
+
 	return GST_DEVICE(g_object_new(GST_TYPE_LIBCAMERA_DEVICE,
 				       /* \todo Use a unique identifier instead of camera name. */
 				       "name", name,
 				       "display-name", name,
 				       "caps", caps,
 				       "device-class", "Source/Video",
+				       "properties", props,
 				       nullptr));
 }
 
