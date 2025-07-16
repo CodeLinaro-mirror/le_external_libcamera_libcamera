09c6e2eb6145b4408c4ff7613403c56b1135e594
Milan Zamazal <mzamazal@redhat.com>
2025-07-16 19:50:22 +0530

    libcamera: simple: Exclude raw configurations from output conversions
    In order to support raw streams, we need to add raw formats to software
    ISP configurations.  In this preparatory patch, the raw formats are
    excluded from output configurations for conversions.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Reviewed-by: Umang Jain <uajain@igalia.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/simple/simple.cpp
+++ src/libcamera/pipeline/simple/simple.cpp
@@ -26,6 +26,7 @@
 
 #include <libcamera/camera.h>
 #include <libcamera/control_ids.h>
+#include <libcamera/pixel_format.h>
 #include <libcamera/request.h>
 #include <libcamera/stream.h>
 
@@ -258,6 +259,12 @@ static const SimplePipelineInfo supportedDevices[] = {
 	{ "qcom-camss", {}, true },
 	{ "sun6i-csi", {}, false },
 };
+
+bool isFormatRaw(const libcamera::PixelFormat &pixFmt)
+{
+	return libcamera::PixelFormatInfo::info(pixFmt).colourEncoding ==
+	       libcamera::PixelFormatInfo::ColourEncodingRAW;
+}
 
 } /* namespace */
 
@@ -1388,7 +1395,7 @@ int SimplePipelineHandler::configure(Camera *camera, CameraConfiguration *c)
 
 		cfg.setStream(&data->streams_[i]);
 
-		if (data->useConversion_)
+		if (data->useConversion_ && !isFormatRaw(cfg.pixelFormat))
 			outputCfgs.push_back(cfg);
 	}
 
