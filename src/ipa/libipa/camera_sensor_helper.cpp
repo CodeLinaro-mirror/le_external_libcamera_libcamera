d450c6bd874897db9ce488ed101358e198fa0bf2
Stefan Klug <stefan.klug@ideasonboard.com>
2025-07-21 15:06:40 +0200

    libipa: camera_sensor_helper: Add quantizeGain() function
    Add a small utility function that calculates the quantized gain that
    gets applied by a sensor when the gain code is set to gainCode(gain).
    This is needed by algorithms to calculate a digital correction gain that
    gets applied to mitigate the error introduce by quantization.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>

--- src/ipa/libipa/camera_sensor_helper.cpp
+++ src/ipa/libipa/camera_sensor_helper.cpp
@@ -132,6 +132,29 @@ double CameraSensorHelper::gain(uint32_t gainCode) const
 }
 
 /**
+ * \brief Quantize the given gain value
+ * \param[in] _gain The real gain
+ * \param[out] quantizationGain The gain that is lost due to quantization
+ *
+ * This function returns the actual gain that get's applied when the sensors
+ * gain is set to gainCode(gain).
+ *
+ * It shall be guaranteed that gainCode(gain) == gainCode(quantizeGain(gain)).
+ *
+ * If \a quantizationGain is provided it is filled with the gain that is lost
+ * due to quantization.
+ *
+ * \return The quantized real gain
+ */
+double CameraSensorHelper::quantizeGain(double _gain, double *quantizationGain) const
+{
+	double g = gain(gainCode(_gain));
+	if (quantizationGain)
+		*quantizationGain = _gain / g;
+	return g;
+}
+
+/**
  * \struct CameraSensorHelper::AnalogueGainLinear
  * \brief Analogue gain constants for the linear gain model
  *
