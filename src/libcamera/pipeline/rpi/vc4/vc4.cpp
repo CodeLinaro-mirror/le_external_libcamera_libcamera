f13d39dd72c763e9519abd74dd4684d08e120983
Milan Zamazal <mzamazal@redhat.com>
2025-07-24 12:38:24 +0200

    config: Look up rpi configuration in the configuration file
    Let's make rpi configuration available in the global configuration file.
    It may be arguable whether pipeline specific configurations belong to
    the global configuration file.  But:
    
    - Having a single configuration file is generally easier for the user.
    - The original configuration via environment variables can be already
      considered global.
    - This option points to other configuration files and it makes little
      sense to add another configuration file to the chain.
    
    The rpi configuration is currently placed in a separate file, specified
    by LIBCAMERA_RPI_CONFIG_FILE environment variable.  Let's support the
    content of the file in the global configuration file.  `version' field
    is omitted as the global configuration file has its own version and the
    required version in the original file has been 1.0 anyway.
    
    The configuration snippet:
    
      configuration:
        pipeline:
          rpi:
            bcm2835:
              pipeline_handler:
                ...
            pisp:
              pipeline_handler:
                ...
    
    To not break current user setups, LIBCAMERA_RPI_CONFIG_FILE environment
    variable is still supported and works as before, pointing to a separate
    configuration file.  Just the duplicate check for the configuration file
    version in platformPipelineConfigure methods is removed.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/rpi/vc4/vc4.cpp
+++ src/libcamera/pipeline/rpi/vc4/vc4.cpp
@@ -61,13 +61,19 @@ public:
 		return unicam_[Unicam::Image].dev();
 	}
 
+	const std::string &target() const override
+	{
+		static const std::string target = "bcm2835";
+		return target;
+	}
+
 	void platformFreeBuffers() override
 	{
 	}
 
 	CameraConfiguration::Status platformValidate(RPi::RPiCameraConfiguration *rpiConfig) const override;
 
-	int platformPipelineConfigure(const std::unique_ptr<YamlObject> &root) override;
+	int platformPipelineConfigure(const YamlObject &phConfig) override;
 
 	void platformStart() override;
 	void platformStop() override;
@@ -493,30 +499,16 @@ CameraConfiguration::Status Vc4CameraData::platformValidate(RPi::RPiCameraConfig
 	return status;
 }
 
-int Vc4CameraData::platformPipelineConfigure(const std::unique_ptr<YamlObject> &root)
+int Vc4CameraData::platformPipelineConfigure(const YamlObject &phConfig)
 {
 	config_ = {
 		.minUnicamBuffers = 2,
 		.minTotalUnicamBuffers = 4,
 	};
 
-	if (!root)
+	if (!phConfig)
 		return 0;
 
-	std::optional<double> ver = (*root)["version"].get<double>();
-	if (!ver || *ver != 1.0) {
-		LOG(RPI, Error) << "Unexpected configuration file version reported";
-		return -EINVAL;
-	}
-
-	std::optional<std::string> target = (*root)["target"].get<std::string>();
-	if (target != "bcm2835") {
-		LOG(RPI, Error) << "Unexpected target reported: expected \"bcm2835\", got "
-				<< (target ? target->c_str() : "(unknown)");
-		return -EINVAL;
-	}
-
-	const YamlObject &phConfig = (*root)["pipeline_handler"];
 	config_.minUnicamBuffers =
 		phConfig["min_unicam_buffers"].get<unsigned int>(config_.minUnicamBuffers);
 	config_.minTotalUnicamBuffers =
