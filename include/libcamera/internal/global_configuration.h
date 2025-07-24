ac98c75ee7f31d6353e7e7cb5bb2de2ae6edf5dc
Milan Zamazal <mzamazal@redhat.com>
2025-07-24 12:38:23 +0200

    config: Add configuration retrieval helpers
    Let's add some helpers to make accessing simple configuration values
    simpler.
    
    GlobalConfiguration::option ensures that no value is returned rather
    than a value of YamlObject::empty.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- include/libcamera/internal/global_configuration.h
+++ include/libcamera/internal/global_configuration.h
@@ -8,6 +8,9 @@
 #pragma once
 
 #include <filesystem>
+#include <optional>
+#include <string>
+#include <string_view>
 
 #include "libcamera/internal/yaml_parser.h"
 
@@ -22,6 +25,11 @@ public:
 
 	unsigned int version() const;
 	Configuration configuration() const;
+	std::optional<std::string> option(
+		const std::initializer_list<std::string_view> confPath) const;
+	std::optional<std::string> envOption(
+		const char *const envVariable,
+		const std::initializer_list<std::string_view> confPath) const;
 
 private:
 	bool loadFile(const std::filesystem::path &fileName);
