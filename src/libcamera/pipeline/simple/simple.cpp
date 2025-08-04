7a32e36de968afce9dc7b24a428cc78ae2f7ac5a
Milan Zamazal <mzamazal@redhat.com>
2025-08-04 18:38:09 +0200

    libcamera: simple: Set the number of software ISP streams to 2
    When software ISP is enabled, we want to be able to provide a raw stream
    in addition to the processed stream.  For this purpose, we need two
    streams.  If only the processed stream is requested, it doesn't harm to
    allocate two.
    
    The number of streams is determined as a camera property in the pipeline
    matching.  To be able to produce both raw and processed output, two
    streams must be provided.  The actual number of streams needed (one or
    two) is determined only when examining roles in
    SimplePipelineHandler::generateConfiguration.
    
    In theory, software ISP could produce multiple processed streams but
    this is out of scope of this patch series.  Hence two streams are
    sufficient at the moment.
    
    When software ISP is not enabled, the camera won't be able to produce
    multiple streams (assuming there's no hardware converter) and only
    single stream should be allocated as before.  The simple pipeline
    handler assumes there's a linear pipeline from the camera sensor to a
    video capture device, and only supports a single stream.  Branches in
    the hardware pipeline that would allow capturing multiple streams from
    the same camera sensor are not supported.  We have no plan to change
    that, as a device that can produce multiple streams will likely be
    better supported by a dedicated pipeline handler.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/simple/simple.cpp
+++ src/libcamera/pipeline/simple/simple.cpp
@@ -1789,6 +1789,16 @@ bool SimplePipelineHandler::matchDevice(MediaDevice *media,
 		}
 	}
 
+	if (info.swIspEnabled) {
+		/*
+		 * When the software ISP is enabled, the simple pipeline handler
+		 * exposes the raw stream, giving a total of two streams. This
+		 * is mutally exclusive with the presence of a converter.
+		 */
+		ASSERT(!converter_);
+		numStreams = 2;
+	}
+
 	swIspEnabled_ = info.swIspEnabled;
 
 	/* Locate the sensors. */
