858c3f2be630fd94a3e328f3017872db56addcb8
Kieran Bingham <kieran.bingham@ideasonboard.com>
2025-09-30 14:26:22 +0200

    libcamera: rkisp1: Only connect delayed controls at start/stop
    The RKISP1 path may potentially have multiple cameras connected through
    complex pipelines such as video multiplexors or multiple FPGA paths.
    
    The RKISP1 pipeline handler notifies DelayedControls that a new frame is
    commencing by using the frameStart event on the ISP and using that to
    signal to DelayedControls that it is time to process the controls for
    the next frame.
    
    When more than one camera is connected to an ISP it is important not to
    signal events to an inactive Camera.
    
    Move the frameStart signal connection from CreateCamera() to start() and
    introduce a corresponding disconnect at stop().
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/rkisp1/rkisp1.cpp
+++ src/libcamera/pipeline/rkisp1/rkisp1.cpp
@@ -1097,6 +1097,11 @@ int PipelineHandlerRkISP1::start(Camera *camera, [[maybe_unused]] const ControlL
 	utils::ScopeExitActions actions;
 	int ret;
 
+	isp_->frameStart.connect(data->delayedCtrls_.get(),
+				 &DelayedControls::applyControls);
+
+	actions += [&]() { isp_->frameStart.disconnect(data->delayedCtrls_.get()); };
+
 	/* Allocate buffers for internal pipeline usage. */
 	ret = allocateBuffers(camera);
 	if (ret)
@@ -1167,6 +1172,8 @@ void PipelineHandlerRkISP1::stopDevice(Camera *camera)
 	int ret;
 
 	isp_->setFrameStartEnabled(false);
+
+	isp_->frameStart.disconnect(data->delayedCtrls_.get());
 
 	data->ipa_->stop();
 
@@ -1354,8 +1361,6 @@ int PipelineHandlerRkISP1::createCamera(MediaEntity *sensor)
 	data->delayedCtrls_ =
 		std::make_unique<DelayedControls>(data->sensor_->device(),
 						  params);
-	isp_->frameStart.connect(data->delayedCtrls_.get(),
-				 &DelayedControls::applyControls);
 
 	uint32_t supportedBlocks = kDefaultExtParamsBlocks;
 
