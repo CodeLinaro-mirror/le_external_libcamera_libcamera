73256a1e5f166d47ba382e858ea2b583f6e9b06e
Paul Elder <paul.elder@ideasonboard.com>
2025-10-07 19:27:44 +0900

    ipa: ipu3, mali-c55, rkisp1, rpi: Fix reporting non-scalar controls
    The ControlInfos of non-scalar controls that are reported in controls()
    must have scalar min/max and non-scalar default values for controls that
    have a defined size. Currently this is relevant to the following
    controls:
    - ColourGains
    - ColourCorrectionMatrix
    - FrameDurationLimits
    
    A mismatch of scalarness in these fields causes deserialization errors
    in ControlSerializer which manifest when IPAs are run in isolation.
    
    Fix the scalarness of these controls where relevant.
    
    Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/ipu3/ipu3.cpp
+++ src/ipa/ipu3/ipu3.cpp
@@ -20,6 +20,7 @@
 
 #include <libcamera/base/file.h>
 #include <libcamera/base/log.h>
+#include <libcamera/base/span.h>
 #include <libcamera/base/utils.h>
 
 #include <libcamera/control_ids.h>
@@ -280,10 +281,11 @@ void IPAIPU3::updateControls(const IPACameraSensorInfo &sensorInfo,
 		uint64_t frameSize = lineLength * frameHeights[i];
 		frameDurations[i] = frameSize / (sensorInfo.pixelRate / 1000000U);
 	}
+	std::array<int64_t, 2> defFrameDurations = { frameDurations[2], frameDurations[2] };
 
 	controls[&controls::FrameDurationLimits] = ControlInfo(frameDurations[0],
 							       frameDurations[1],
-							       frameDurations[2]);
+							       Span<const int64_t, 2>{ defFrameDurations });
 
 	controls.merge(context_.ctrlMap);
 	*ipaControls = ControlInfoMap(std::move(controls), controls::controls);
