fae2ad89211a20607b8454286edbe86430a72239
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2025-10-07 20:17:46 +0200

    ipa: libipa: Introduce V4L2Params
    The existing RkISP1Params helper class allows the RkISP1 IPA to handle
    both the extensible parameters format and the legacy fixed-size format.
    
    With the introduction of v4l2-isp.h in the Linux kernel the part of
    the RkISP1Params helper class that handles extensible parameters can
    be generalized so that other IPA modules can use the same helpers
    to populate a parameters buffer compatible with v4l2-isp.h.
    
    Generalize the RkISP1Params class to a new libipa component named
    V4L2Params and derive the existing RkISP1Params from it, leaving
    in the RkISP1-specific implementation the handling of the legacy format.
    
    Deriving RkISP1Params from V4L2Params requires changing the size
    associated to each block to include the size of v4l2_params_block_header
    in the ipa:rkisp1::kBlockTypeInfo map as the V4L2Params::block()
    implementation doesn't account for that as RkIS1Params::block()
    implementation did.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Tested-by: Antoine Bouyer <antoine.bouyer@nxp.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/ipa/libipa/v4l2_params.h
@@ -0,0 +1,142 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2025, Ideas On Board
+ *
+ * V4L2 Parameters
+ */
+
+#pragma once
+
+#include <map>
+#include <stdint.h>
+#include <string.h>
+
+#include <linux/media/v4l2-isp.h>
+
+#include <libcamera/base/span.h>
+
+namespace libcamera {
+
+namespace ipa {
+
+template<typename T>
+class V4L2ParamsBlock
+{
+public:
+	V4L2ParamsBlock(const Span<uint8_t> data)
+		: data_(data)
+	{
+	}
+
+	virtual ~V4L2ParamsBlock() {}
+
+	virtual void setEnabled(bool enabled)
+	{
+		struct v4l2_isp_params_block_header *header =
+			reinterpret_cast<struct v4l2_isp_params_block_header *>(data_.data());
+
+		header->flags &= ~(V4L2_ISP_PARAMS_FL_BLOCK_ENABLE |
+				   V4L2_ISP_PARAMS_FL_BLOCK_DISABLE);
+		header->flags |= enabled ? V4L2_ISP_PARAMS_FL_BLOCK_ENABLE
+					 : V4L2_ISP_PARAMS_FL_BLOCK_DISABLE;
+	}
+
+	virtual const T *operator->() const
+	{
+		return reinterpret_cast<const T *>(data_.data());
+	}
+
+	virtual T *operator->()
+	{
+		return reinterpret_cast<T *>(data_.data());
+	}
+
+	virtual const T &operator*() const
+	{
+		return *reinterpret_cast<const T *>(data_.data());
+	}
+
+	virtual T &operator*()
+	{
+		return *reinterpret_cast<T *>(data_.data());
+	}
+
+protected:
+	Span<uint8_t> data_;
+};
+
+template<typename Traits>
+class V4L2Params
+{
+public:
+	V4L2Params(Span<uint8_t> data, unsigned int version)
+		: data_(data)
+	{
+		struct v4l2_isp_params_buffer *cfg =
+			reinterpret_cast<struct v4l2_isp_params_buffer *>(data_.data());
+		cfg->data_size = 0;
+		cfg->version = version;
+		used_ = offsetof(struct v4l2_isp_params_buffer, data);
+	}
+
+	size_t size() const { return used_; }
+
+	template<typename Traits::id_type Id>
+	auto block()
+	{
+		using Details = typename Traits::template id_to_details<Id>;
+
+		using Type = typename Details::type;
+		constexpr auto kernelId = Details::blockType;
+
+		auto data = block(Id, kernelId, sizeof(Type));
+		return V4L2ParamsBlock<Type>(data);
+	}
+
+protected:
+	Span<uint8_t> block(typename Traits::id_type type,
+			    unsigned int blockType, size_t blockSize)
+	{
+		/*
+		 * Look up the block in the cache first. If an algorithm
+		 * requests the same block type twice, it should get the same
+		 * block.
+		 */
+		auto cacheIt = blocks_.find(type);
+		if (cacheIt != blocks_.end())
+			return cacheIt->second;
+
+		/* Make sure we don't run out of space. */
+		if (blockSize > data_.size() - used_)
+			return {};
+
+		/* Allocate a new block, clear its memory, and initialize its header. */
+		Span<uint8_t> block = data_.subspan(used_, blockSize);
+		used_ += blockSize;
+
+		struct v4l2_isp_params_buffer *cfg =
+			reinterpret_cast<struct v4l2_isp_params_buffer *>(data_.data());
+		cfg->data_size += blockSize;
+
+		memset(block.data(), 0, block.size());
+
+		struct v4l2_isp_params_block_header *header =
+			reinterpret_cast<struct v4l2_isp_params_block_header *>(block.data());
+		header->type = blockType;
+		header->size = block.size();
+
+		/* Update the cache. */
+		blocks_[type] = block;
+
+		return block;
+	}
+
+	Span<uint8_t> data_;
+	size_t used_;
+
+	std::map<typename Traits::id_type, Span<uint8_t>> blocks_;
+};
+
+} /* namespace ipa */
+
+} /* namespace libcamera */
