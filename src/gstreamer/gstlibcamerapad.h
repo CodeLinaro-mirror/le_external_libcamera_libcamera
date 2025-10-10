f360edd89f0e18fd5e36f45a3bf432c42e3f06e1
Umang Jain <uajain@igalia.com>
2025-10-10 19:48:25 +0530

    gstreamer: Associate libcamera::Stream with GstPad
    Instead of trying to figure out which libcamera::Stream, the GstPad
    is configured with (from the libcamera pool), associate the Stream
    with the GstPad directly using gst_pad_set_element_private().
    
    The libcamera::Stream can now be obtained using
    gst_pad_get_element_private() hence, drop the getter
    gst_libcamera_pad_get_stream().
    
    Additionally, drop retrieving the stream configuration
    using the same index, as of the srcpads_ vector in processRequest().
    This can break in future, if CameraConfiguration moves away
    from vector for storing stream configurations. Hence, retrieve
    the stream configuration correctly, from GstPad's configured stream.
    
    Signed-off-by: Umang Jain <uajain@igalia.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/gstreamer/gstlibcamerapad.h
+++ src/gstreamer/gstlibcamerapad.h
@@ -31,6 +31,4 @@ GstVideoInfo gst_libcamera_pad_get_video_info(GstPad *pad);
 
 void gst_libcamera_pad_set_video_info(GstPad *pad, const GstVideoInfo *info);
 
-libcamera::Stream *gst_libcamera_pad_get_stream(GstPad *pad);
-
 void gst_libcamera_pad_set_latency(GstPad *pad, GstClockTime latency);
