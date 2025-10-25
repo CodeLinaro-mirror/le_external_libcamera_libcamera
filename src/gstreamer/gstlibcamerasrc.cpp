37a99ee055da8f340a1c37655dea17b5430b184a
Umang Jain <uajain@igalia.com>
2025-10-25 15:22:57 +0100

    gstreamer: Shorten function name
    Shorten the following utility function name:
    	gst_libcamera_stream_configuration_to_caps()
    to
    	gst_libcamera_stream_cfg_to_caps()
    
    Comestic fix only for slight readability enhancement.
    
    Signed-off-by: Umang Jain <uajain@igalia.com>
    Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Reviewed-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/gstreamer/gstlibcamerasrc.cpp
+++ src/gstreamer/gstlibcamerasrc.cpp
@@ -639,7 +639,7 @@ gst_libcamera_src_negotiate(GstLibcameraSrc *self)
 		GstPad *srcpad = state->srcpads_[i];
 		const StreamConfiguration &stream_cfg = state->config_->at(i);
 
-		g_autoptr(GstCaps) caps = gst_libcamera_stream_configuration_to_caps(stream_cfg, transfer[i]);
+		g_autoptr(GstCaps) caps = gst_libcamera_stream_cfg_to_caps(stream_cfg, transfer[i]);
 		gst_libcamera_framerate_to_caps(caps, element_caps);
 
 		if (status == CameraConfiguration::Adjusted &&
@@ -667,7 +667,7 @@ gst_libcamera_src_negotiate(GstLibcameraSrc *self)
 		GstBufferPool *video_pool = nullptr;
 		GstVideoInfo info;
 
-		g_autoptr(GstCaps) caps = gst_libcamera_stream_configuration_to_caps(stream_cfg, transfer[i]);
+		g_autoptr(GstCaps) caps = gst_libcamera_stream_cfg_to_caps(stream_cfg, transfer[i]);
 
 		gst_video_info_from_caps(&info, caps);
 		gst_libcamera_pad_set_video_info(srcpad, &info);
