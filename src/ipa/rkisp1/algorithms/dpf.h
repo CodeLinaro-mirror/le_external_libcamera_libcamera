7f81e2327f98ed57c746922d6eb31d606f3573ba
Rui Wang <rui.wang@ideasonboard.com>
2025-10-28 17:17:43 -0400

    ipa: rkisp1: algorithms: dpf: collect DPF manual overrides
    Implement collectManualOverrides() to capture strength and developer-mode
    override arrays from control requests. Extend queueRequest() to collect
    overrides in manual mode and mark frames dirty when strength values differ
    from the active configuration.
    
    Developer mode controls (spatial coefficients, filter size, NLL tables)
    are only collected when devmode is enabled. The function populates the
    overrides_ structure which will be applied during prepare().
    
    Signed-off-by: Rui Wang <rui.wang@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/rkisp1/algorithms/dpf.h
+++ src/ipa/rkisp1/algorithms/dpf.h
@@ -34,23 +34,53 @@ public:
 private:
 	struct rkisp1_cif_isp_dpf_config config_ {
 	};
+
 	struct rkisp1_cif_isp_dpf_strength_config strengthConfig_ {
 	};
+
 	struct rkisp1_cif_isp_dpf_config baseConfig_ {
 	};
+
 	struct rkisp1_cif_isp_dpf_strength_config baseStrengthConfig_ {
+	};
+
+	struct DpfStrengthSettings {
+		uint16_t r, g, b;
+	};
+
+	struct DpfSpatialGreenSettings {
+		std::array<uint8_t, RKISP1_CIF_ISP_DPF_MAX_SPATIAL_COEFFS> coeffs;
+	};
+
+	struct DpfSpatialRbSettings {
+		std::array<uint8_t, RKISP1_CIF_ISP_DPF_MAX_SPATIAL_COEFFS> coeffs;
+		uint8_t size; // 0=9x9, 1=13x9
+	};
+
+	struct DpfNllSettings {
+		std::array<uint16_t, RKISP1_CIF_ISP_DPF_MAX_NLF_COEFFS> coeffs;
+		uint8_t scaleMode; // 0 linear, 1 log
 	};
 	struct IsoLevelConfig {
 		unsigned int maxIso; /* inclusive upper bound */
 		struct rkisp1_cif_isp_dpf_config dpf;
 		struct rkisp1_cif_isp_dpf_strength_config strength;
 	};
+	struct Overrides {
+		std::optional<DpfStrengthSettings> strength;
+		std::optional<DpfSpatialGreenSettings> spatialGreen;
+		std::optional<DpfSpatialRbSettings> spatialRb;
+		std::optional<uint8_t> rbSize;
+		std::optional<DpfNllSettings> nll;
+		void clear() { *this = Overrides{}; }
+	} overrides_;
 
 	std::vector<IsoLevelConfig> isoLevels_;
 	bool useIsoLevels_ = false;
 	bool enableDpf_ = true; /* YAML master enable */
 
 	void handleEnableControl(const ControlList &controls, IPAFrameContext &frameContext, IPAContext &context) override;
+	void collectManualOverrides(const ControlList &controls) override;
 	bool parseConfig(const YamlObject &tuningData) override;
 	bool parseSingleConfig(const YamlObject &config,
 			       rkisp1_cif_isp_dpf_config &cfg,
