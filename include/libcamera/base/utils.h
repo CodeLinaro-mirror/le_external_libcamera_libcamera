6789bd78e57d10051c08f062ef15169113c4149d
Kieran Bingham <kieran.bingham@ideasonboard.com>
2025-10-30 18:11:27 +0000

    libcamera: base: utils: Simplify hex adaptor
    The libcamera hex string adaptor specifies and casts each type
    specifically to map the size of each type.
    
    This needlessly repeats itself for each type and further more has a bug
    with signed integer extension which causes values such as 0x80 to be
    printed as 0xffffffffffffff80 instead.
    
    Remove the template specialisations for each type, and unify with a
    single templated constructor of the struct hex trait.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- include/libcamera/base/utils.h
+++ include/libcamera/base/utils.h
@@ -78,67 +78,21 @@ struct timespec duration_to_timespec(const duration &value);
 std::string time_point_to_string(const time_point &time);
 
 #ifndef __DOXYGEN__
-struct _hex {
+struct hex {
 	uint64_t v;
 	unsigned int w;
+
+	template<typename T, std::enable_if_t<std::is_integral<T>::value> * = nullptr>
+	hex(T value, unsigned int width = 0)
+	: v(static_cast<std::make_unsigned_t<T>>(value)),
+	  w(width ? width : sizeof(T) * 2)
+	{ }
 };
 
 std::basic_ostream<char, std::char_traits<char>> &
-operator<<(std::basic_ostream<char, std::char_traits<char>> &stream, const _hex &h);
-#endif
-
-template<typename T,
-	 std::enable_if_t<std::is_integral<T>::value> * = nullptr>
-_hex hex(T value, unsigned int width = 0);
-
-#ifndef __DOXYGEN__
-template<>
-inline _hex hex<int8_t>(int8_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 2 };
-}
-
-template<>
-inline _hex hex<uint8_t>(uint8_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 2 };
-}
-
-template<>
-inline _hex hex<int16_t>(int16_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 4 };
-}
-
-template<>
-inline _hex hex<uint16_t>(uint16_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 4 };
-}
-
-template<>
-inline _hex hex<int32_t>(int32_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 8 };
-}
-
-template<>
-inline _hex hex<uint32_t>(uint32_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 8 };
-}
-
-template<>
-inline _hex hex<int64_t>(int64_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 16 };
-}
-
-template<>
-inline _hex hex<uint64_t>(uint64_t value, unsigned int width)
-{
-	return { static_cast<uint64_t>(value), width ? width : 16 };
-}
+operator<<(std::basic_ostream<char, std::char_traits<char>> &stream, const struct hex &h);
+#else
+void hex(T value, unsigned int width = 0);
 #endif
 
 size_t strlcpy(char *dst, const char *src, size_t size);
--- src/libcamera/base/utils.cpp
+++ src/libcamera/base/utils.cpp
@@ -187,7 +187,7 @@ std::string time_point_to_string(const time_point &time)
 }
 
 std::basic_ostream<char, std::char_traits<char>> &
-operator<<(std::basic_ostream<char, std::char_traits<char>> &stream, const _hex &h)
+operator<<(std::basic_ostream<char, std::char_traits<char>> &stream, const struct hex &h)
 {
 	stream << "0x";
 
