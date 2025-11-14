718d34b94f32853293593c70e3d736d410ffff16
Kieran Bingham <kieran.bingham@ideasonboard.com>
2025-11-14 00:54:24 +0000

    ipa: libipa: fixedpoint: Move float conversion inline
    With all users of the floatingToFixedPoint and fixedToFloatingPoint
    calls converted to use the FixedPoint Quantized types, remove the calls
    and documentation and move the implementation inline in the
    FixedPointTraits implementation.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/libipa/fixedpoint.h
+++ src/ipa/libipa/fixedpoint.h
@@ -16,55 +16,6 @@ namespace libcamera {
 
 namespace ipa {
 
-#ifndef __DOXYGEN__
-template<unsigned int I, unsigned int F, typename R, typename T,
-	 std::enable_if_t<std::is_integral_v<R> &&
-			  std::is_floating_point_v<T>> * = nullptr>
-#else
-template<unsigned int I, unsigned int F, typename R, typename T>
-#endif
-constexpr R floatingToFixedPoint(T number)
-{
-	static_assert(sizeof(int) >= sizeof(R));
-	static_assert(I + F <= sizeof(R) * 8);
-
-	/*
-	 * The intermediate cast to int is needed on arm platforms to properly
-	 * cast negative values. See
-	 * https://embeddeduse.com/2013/08/25/casting-a-negative-float-to-an-unsigned-int/
-	 */
-	R mask = (1 << (F + I)) - 1;
-	R frac = static_cast<R>(static_cast<int>(std::round(number * (1 << F)))) & mask;
-
-	return frac;
-}
-
-#ifndef __DOXYGEN__
-template<unsigned int I, unsigned int F, typename R, typename T,
-	 std::enable_if_t<std::is_floating_point_v<R> &&
-			  std::is_integral_v<T>> * = nullptr>
-#else
-template<unsigned int I, unsigned int F, typename R, typename T>
-#endif
-constexpr R fixedToFloatingPoint(T number)
-{
-	static_assert(sizeof(int) >= sizeof(T));
-	static_assert(I + F <= sizeof(T) * 8);
-
-	if constexpr (std::is_unsigned_v<T>)
-		return static_cast<R>(number) / static_cast<R>(1 << F);
-
-	/*
-	 * Recreate the upper bits in case of a negative number by shifting the sign
-	 * bit from the fixed point to the first bit of the unsigned and then right shifting
-	 * by the same amount which keeps the sign bit in place.
-	 * This can be optimized by the compiler quite well.
-	 */
-	int remaining_bits = sizeof(int) * 8 - (I + F);
-	int t = static_cast<int>(static_cast<unsigned>(number) << remaining_bits) >> remaining_bits;
-	return static_cast<R>(t) / static_cast<R>(1 << F);
-}
-
 template<unsigned int I, unsigned int F, typename T>
 struct FixedPointQTraits {
 	static_assert(std::is_integral_v<T>, "FixedPointQTraits: T must be integral");
@@ -88,17 +39,36 @@ struct FixedPointQTraits {
 
 	static constexpr float toFloat(QuantizedType q)
 	{
-		return fixedToFloatingPoint<I, F, float, QuantizedType>(q);
+		if constexpr (std::is_unsigned_v<T>)
+			return static_cast<float>(q) / static_cast<float>(1 << F);
+
+		/*
+		 * Recreate the upper bits in case of a negative number by shifting the sign
+		 * bit from the fixed point to the first bit of the unsigned and then right shifting
+		 * by the same amount which keeps the sign bit in place.
+		 * This can be optimized by the compiler quite well.
+		 */
+		static_assert(sizeof(int) >= sizeof(T));
+
+		int remaining_bits = sizeof(int) * 8 - (I + F);
+		int t = static_cast<int>(static_cast<unsigned>(q) << remaining_bits) >> remaining_bits;
+		return static_cast<float>(t) / static_cast<float>(1 << F);
 	}
 
-	static constexpr float min = fixedToFloatingPoint<I, F, float>(qmin);
-	static constexpr float max = fixedToFloatingPoint<I, F, float>(qmax);
+	static constexpr float min = toFloat(qmin);
+	static constexpr float max = toFloat(qmax);
 
 	/* Conversion functions required by Quantized<Traits> */
 	static constexpr QuantizedType fromFloat(float v)
 	{
 		v = std::clamp(v, min, max);
-		return floatingToFixedPoint<I, F, QuantizedType, float>(v);
+
+		/*
+		 * The intermediate cast to int is needed on arm platforms to properly
+		 * cast negative values. See
+		 * https://embeddeduse.com/2013/08/25/casting-a-negative-float-to-an-unsigned-int/
+		 */
+		return static_cast<T>(static_cast<int>(std::round(v * (1 << F)))) & BitMask;
 	}
 };
 
