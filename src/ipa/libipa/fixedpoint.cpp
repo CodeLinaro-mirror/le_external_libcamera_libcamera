41d2abd9d59261ae878a4c41b7d0c930bf3c6f6c
Kieran Bingham <kieran.bingham@ideasonboard.com>
2025-11-14 00:54:11 +0000

    ipa: libipa: Provide Q5.4 FixedPoint support
    Provide a FixedPoint Quantized type to support the Q5.4 format
    representing values in the range [-16.0, 15.9375] with a resolution of
    1/16.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/libipa/fixedpoint.cpp
+++ src/ipa/libipa/fixedpoint.cpp
@@ -140,6 +140,15 @@ namespace ipa {
  */
 
 /**
+ * \typedef Q5_4
+ * \brief 5.4 signed fixed-point quantizer
+ *
+ * A Quantized type using 5 bits for the integer part and 4 bits for the
+ * fractional part, stored in a signed 16-bit integer (\c int16_t). Represents
+ * values in the range [-16.0, 15.9375] with a resolution of 1/16.
+ */
+
+/**
  * \typedef Q12_4
  * \brief 12.4 signed fixed-point quantizer
  *
