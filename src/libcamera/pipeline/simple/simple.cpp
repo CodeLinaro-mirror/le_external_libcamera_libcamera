3e4800d13432e7d05bb1fa9d194d2c321ebc1aef
Milan Zamazal <mzamazal@redhat.com>
2025-11-27 22:19:27 +0100

    libcamera: simple: Require metadata only when software ISP is used
    If software ISP is enabled then metadata is required in the simple
    pipeline.  But this doesn't apply if the software ISP is not actually
    used, for example when only a raw stream is produced.  Then the pipeline
    waits for metadata that never comes.
    
    This patch fixes the problem by requiring metadata only when software
    ISP is used.
    
    Reviewed-by: Umang Jain <uajain@igalia.com>
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/pipeline/simple/simple.cpp
+++ src/libcamera/pipeline/simple/simple.cpp
@@ -1697,6 +1697,7 @@ int SimplePipelineHandler::queueRequestDevice(Camera *camera, Request *request)
 	int ret;
 
 	std::map<const Stream *, FrameBuffer *> buffers;
+	bool metadataRequired = false;
 
 	for (auto &[stream, buffer] : request->buffers()) {
 		/*
@@ -1706,6 +1707,7 @@ int SimplePipelineHandler::queueRequestDevice(Camera *camera, Request *request)
 		 */
 		if (data->useConversion_) {
 			buffers.emplace(stream, buffer);
+			metadataRequired = !!data->swIsp_;
 		} else {
 			ret = data->video_->queueBuffer(buffer);
 			if (ret < 0)
@@ -1713,7 +1715,7 @@ int SimplePipelineHandler::queueRequestDevice(Camera *camera, Request *request)
 		}
 	}
 
-	data->frameInfo_.create(request, !!data->swIsp_);
+	data->frameInfo_.create(request, metadataRequired);
 	if (data->useConversion_) {
 		data->conversionQueue_.push({ request, std::move(buffers) });
 		if (data->swIsp_)
