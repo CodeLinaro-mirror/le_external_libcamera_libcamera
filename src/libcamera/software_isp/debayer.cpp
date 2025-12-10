46e501c394123e8b8ed8fd047c41aeef9f6f0948
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2025-12-10 00:31:32 +0000

    libcamera: software_isp: debayer: Introduce a start() / stop() methods to the debayer object
    In order to initialise and deinitialise gpuisp we need to be able to setup
    EGL in the same thread as Debayer::process() happens in.
    
    In order to do this we need to extend the Debayer object to provide start
    and stop methods which are triggered through invokeMethod in the same way
    as process() is.
    
    Introduce start() and stop() methods to the debayer class. Trigger those
    methods as described above via invokeMethod. The debayer_egl class will
    take care of initialising and de-initialising as necessary. Debayer CPU
    sees no functional change.
    
    [bod: Made method blocking not queued per Robert's bugfixes]
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/software_isp/debayer.cpp
+++ src/libcamera/software_isp/debayer.cpp
@@ -337,6 +337,24 @@ Debayer::~Debayer()
  */
 
 /**
+ * \fn int Debayer::start()
+ * \brief Execute a start signal in the debayer object from workerthread context.
+ *
+ * In the DebayerCPU case this is an empty stub function but
+ * for the GPU case this does something useful. The stub here is to
+ * ensure the right version of the virtual gets called.
+ */
+
+/**
+ * \fn void Debayer::stop()
+ * \brief Stop the debayering process and perform cleanup
+ *
+ * In the DebayerCPU case this is an empty stub function but
+ * for the GPU case this does something useful. The stub here is to
+ * ensure the right version of the virtual gets called.
+ */
+
+/**
  * \fn void Debayer::setParams(DebayerParams &params)
  * \brief Select the bayer params to use for the next frame debayer
  * \param[in] params The parameters to be used in debayering
