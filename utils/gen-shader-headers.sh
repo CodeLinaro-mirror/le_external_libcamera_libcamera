raspberrypi/libcamera

commit 19371dee4146b72e11b44cb1b7ac7b7570cc4306

utils: gen-shader-headers: Add a utility to generate headers from shaders
Two simple script to generate a header that contains GLSL shaders translated
to C arrays.

Acked-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Milan Zamazal <mzamazal@redhat.com>
Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

utils/gen-shader-headers.sh
@@ -1,0 +1,55 @@
+#!/bin/sh
+
+set -e
+
+usage() {
+	echo "Usage: $0 <src_dir> <build_dir> <output_header_name> <shader_file1> [shader_file2 ...]"
+	echo
+	echo "Generates a C header file containing hex-encoded shader data."
+	echo
+	echo "Arguments:"
+	echo "  src_dir             Path to the base of the source directory"
+	echo "  build_dir           Directory where shader files are located and header will be written"
+	echo "  output_header_name  Name of the generated header file (relative to build_dir)"
+	echo "  shader_file(s)      One or more shader files to embed in the header"
+	exit 1
+}
+
+if [ $# -lt 4 ]; then
+	echo "Error: Invalid argument count."
+	usage
+fi
+
+src_dir="$1"; shift
+build_dir="$1"; shift
+build_path=$build_dir/"$1"; shift
+
+cat <<EOF > "$build_path"
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/* This file is auto-generated, do not edit! */
+
+#pragma once
+
+EOF
+
+cat <<EOF >> "$build_path"
+/*
+ * List the names of the shaders at the top of
+ * header for readability's sake
+ *
+EOF
+
+for file in "$@"; do
+	name=$(basename "$build_dir/$file" | tr '.' '_')
+	echo "[SHADER-GEN] $name"
+	echo " * unsigned char $name;" >> "$build_path"
+done
+
+echo "*/" >> "$build_path"
+
+echo "/* Hex encoded shader data */" >> "$build_path"
+for file in "$@"; do
+	name=$(basename "$build_dir/$file")
+	"$src_dir/utils/gen-shader-header.py" "$name" "$build_dir/$file" >> "$build_path"
+	echo >> "$build_path"
+done

utils/gen-shader-header.py
@@ -1,0 +1,37 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2025, Bryan O'Donoghue.
+#
+# Author: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
+#
+# A Python script which takes a list of shader files and converts each of them
+# into a C header.
+#
+import sys
+
+try:
+    with open(sys.argv[2], "rb") as file:
+        data = file.read()
+        data_len = len(data)
+
+        name = sys.argv[1].replace(".", "_")
+        name_len = name + "_len"
+
+        j = 0
+        print("unsigned char const", name, "[] = {")
+        for ch in data:
+            print(f"0x{ch:02x}, ", end="")
+            j = (j + 1) % 16
+            if j == 0:
+                print()
+        if j != 0:
+            print()
+        print("};")
+
+        print()
+        print(f"const unsigned int {name_len}={data_len};")
+
+except FileNotFoundError:
+    print(f"File {sys.argv[2]} not found", file=sys.stderr)
+except IOError:
+    print(f"Unable to read {sys.argv[2]}", file=sys.stderr)

CodeLinaro-mirror/le_external_libcamera_libcamera

commit b9e0f624b2ae35d7c28a0ed52e733f1b4fa1e284

utils: gen-shader-headers: Fix subproject build
Meson already takes care of passing the proper absolute or relative
paths to commands. There is no need do more path manipulation.

So simplify the script by using the paths as-is. This also fixes the
path manipulation issue that prevented libcamera from building as a
subproject.

Fixes: 19371dee4146b7 ("utils: gen-shader-headers: Add a utility to generate headers from shaders")
Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/meson.build
@@ -188,7 +188,7 @@ libcamera_shader_headers = custom_target(
     'gen-shader-headers',
     input : [shader_files],
     output : 'glsl_shaders.h',
-    command : [gen_shader_headers, meson.project_source_root(), meson.project_build_root(), '@OUTPUT@', '@INPUT@'],
+    command : [gen_shader_headers, meson.project_source_root(), '@OUTPUT@', '@INPUT@'],
 )
 
 libcamera_internal_headers += libcamera_shader_headers

utils/gen-shader-headers.sh
@@ -3,14 +3,13 @@
 set -e
 
 usage() {
-	echo "Usage: $0 <src_dir> <build_dir> <output_header_name> <shader_file1> [shader_file2 ...]"
+	echo "Usage: $0 <src_dir> <output_header> <shader_file1> [shader_file2 ...]"
 	echo
 	echo "Generates a C header file containing hex-encoded shader data."
 	echo
 	echo "Arguments:"
 	echo "  src_dir             Path to the base of the source directory"
-	echo "  build_dir           Directory where shader files are located and header will be written"
-	echo "  output_header_name  Name of the generated header file (relative to build_dir)"
+	echo "  output_header       Path to the generated header file"
 	echo "  shader_file(s)      One or more shader files to embed in the header"
 	exit 1
 }
@@ -21,8 +20,7 @@ if [ $# -lt 4 ]; then
 fi
 
 src_dir="$1"; shift
-build_dir="$1"; shift
-build_path=$build_dir/"$1"; shift
+build_path="$1"; shift
 
 cat <<EOF > "$build_path"
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
@@ -40,7 +38,7 @@ cat <<EOF >> "$build_path"
 EOF
 
 for file in "$@"; do
-	name=$(basename "$build_dir/$file" | tr '.' '_')
+	name=$(basename "$file" | tr '.' '_')
 	echo "[SHADER-GEN] $name"
 	echo " * unsigned char $name;" >> "$build_path"
 done
@@ -49,7 +47,7 @@ echo "*/" >> "$build_path"
 
 echo "/* Hex encoded shader data */" >> "$build_path"
 for file in "$@"; do
-	name=$(basename "$build_dir/$file")
-	"$src_dir/utils/gen-shader-header.py" "$name" "$build_dir/$file" >> "$build_path"
+	name=$(basename "$file")
+	"$src_dir/utils/gen-shader-header.py" "$name" "$file" >> "$build_path"
 	echo >> "$build_path"
 done