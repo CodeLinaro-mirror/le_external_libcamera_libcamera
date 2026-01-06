0aac88c0b1c427b815a68a7958073914f0875e0e
Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
2026-01-06 17:57:35 +0100

    libcamera: base: Add file for C++20 polyfills
    Add `cxx20.h` that will contain (possibly limited) C++17 implementations
    of some C++20 standard library features.
    
    Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Acked-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ include/libcamera/base/internal/cxx20.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2025, Ideas on Board Oy
+ *
+ * C++20 polyfills
+ */
+
+#pragma once
+
+/**
+ * \internal
+ * \file cxx20.h
+ * \brief C++17 implementations of certain C++20 types and functions
+ */
+
+namespace libcamera::internal::cxx20 {
+
+} /* namespace libcamera::internal::cxx20 */
