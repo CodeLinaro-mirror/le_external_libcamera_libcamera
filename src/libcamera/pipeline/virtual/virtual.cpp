raspberrypi/libcamera

commit 5d2d005306ad1b64c7ad857c48a2edf8e56629f5

pipeline: virtual: Provide and validate colorspace
Virtual pipeline handler should provide colorSpace in
generateConfiguration() and validate the colorspace in validate().
It is mandatory for a pipeline handler to set the colorspace if it
is unset in the stream configuration, during validate().

For choosing the colorspace for the generated NV12 frames, following
points have been taken into account:
- The transfer function should be Rec.709 for NV12
- The YCbCr encoding has been chosen Rec.709 as it is the most common
  than Rec.601/Rec.2020
- Range should be 'Limited' as with the NV12 pixel format.

Hence, the closest colorspace match is ColorSpace::Rec709 which is
set for the virtual pipeline handler.

Signed-off-by: Umang Jain <uajain@igalia.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Tested-by: Robert Mader <robert.mader@collabora.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/pipeline/virtual/virtual.cpp
@@ -214,6 +214,17 @@ CameraConfiguration::Status VirtualCameraConfiguration::validate()
 			adjusted = true;
 		}
 
+		if (cfg.colorSpace != ColorSpace::Rec709) {
+			cfg.colorSpace = ColorSpace::Rec709;
+			status = Adjusted;
+			adjusted = true;
+		}
+
+		if (validateColorSpaces() == Adjusted) {
+			status = Adjusted;
+			adjusted = true;
+		}
+
 		if (adjusted)
 			LOG(Virtual, Info)
 				<< "Stream configuration adjusted to " << cfg.toString();
@@ -278,6 +289,7 @@ PipelineHandlerVirtual::generateConfiguration(Camera *camera,
 		cfg.pixelFormat = pixelFormat;
 		cfg.size = data->config_.maxResolutionSize;
 		cfg.bufferCount = VirtualCameraConfiguration::kBufferCount;
+		cfg.colorSpace = ColorSpace::Rec709;
 
 		config->addConfiguration(cfg);
 	}

CodeLinaro-mirror/le_external_libcamera_libcamera

commit 51df76db9098cdf324ec0f2ffce73a34c2212893

pipeline: virtual: Rectify validation of colorspace
Now that each frame generator class supports the colorspace
information using the colorspace() getter, use that to validate
(or set) the colorspace of a particular stream configuration instead
of a incorrect hard-coded one.

Fixes: 5d2d005306ad ("pipeline: virtual: Provide and validate colorspace")
Signed-off-by: Umang Jain <uajain@igalia.com>
Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/pipeline/virtual/virtual.cpp
@@ -185,7 +185,7 @@ CameraConfiguration::Status VirtualCameraConfiguration::validate()
 		status = Adjusted;
 	}
 
-	for (StreamConfiguration &cfg : config_) {
+	for (auto [i, cfg] : utils::enumerate(config_)) {
 		bool adjusted = false;
 		bool found = false;
 		for (const auto &resolution : data_->config_.resolutions) {
@@ -214,8 +214,9 @@ CameraConfiguration::Status VirtualCameraConfiguration::validate()
 			adjusted = true;
 		}
 
-		if (cfg.colorSpace != ColorSpace::Rec709) {
-			cfg.colorSpace = ColorSpace::Rec709;
+		const ColorSpace colorspace = data_->streamConfigs_[i].frameGenerator->colorspace();
+		if (cfg.colorSpace != colorspace) {
+			cfg.colorSpace = colorspace;
 			status = Adjusted;
 			adjusted = true;
 		}
@@ -289,7 +290,6 @@ PipelineHandlerVirtual::generateConfiguration(Camera *camera,
 		cfg.pixelFormat = pixelFormat;
 		cfg.size = data->config_.maxResolutionSize;
 		cfg.bufferCount = VirtualCameraConfiguration::kBufferCount;
-		cfg.colorSpace = ColorSpace::Rec709;
 
 		config->addConfiguration(cfg);
 	}