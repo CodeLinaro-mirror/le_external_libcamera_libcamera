raspberrypi/libcamera

commit 82ed6c19c2b362b268536ac123d8d03a3222a23d

libcamera: ipa: simple: Initialise the general correction matrix
The combined matrix must be reset to the initial value before each frame
is prepared.  This must be done outside algorithms because any of the
algorithms may be disabled while the matrix must be always initialised.

Let's initialise the combined matrix to the identity matrix (which keeps
the pixel values unchanged) in software ISP just before calling
`prepare' on the algorithms.

Matrix updates can no longer be skipped in ccm.cpp, otherwise the CCM
won't be applied if there is no temperature or saturation change.  We
explicitly track whether the CCM has been set up completely rather than
relying on the frame number, to avoid missing the initialisation in case
the first frame is skipped due to some error.

Reviewed-by: Robert Mader <robert.mader@collabora.com>
Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
Acked-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/ipa/simple/algorithms/ccm.cpp
@@ -83,7 +83,7 @@ void Ccm::applySaturation(Matrix<float, 3, 3> &ccm, float saturation)
 	ccm = ycbcr2rgb * saturationMatrix * rgb2ycbcr * ccm;
 }
 
-void Ccm::prepare(IPAContext &context, const uint32_t frame,
+void Ccm::prepare(IPAContext &context, [[maybe_unused]] const uint32_t frame,
 		  IPAFrameContext &frameContext, [[maybe_unused]] DebayerParams *params)
 {
 	auto &saturation = context.activeState.knobs.saturation;
@@ -91,24 +91,21 @@ void Ccm::prepare(IPAContext &context, const uint32_t frame,
 	const unsigned int ct = context.activeState.awb.temperatureK;
 
 	/* Change CCM only on saturation or bigger temperature changes. */
-	if (frame > 0 &&
-	    utils::abs_diff(ct, lastCt_) < kTemperatureThreshold &&
-	    saturation == lastSaturation_) {
-		frameContext.ccm = context.activeState.ccm;
-		return;
+	if (!currentCcm_ ||
+	    utils::abs_diff(ct, lastCt_) >= kTemperatureThreshold ||
+	    saturation != lastSaturation_) {
+		currentCcm_ = ccm_.getInterpolated(ct);
+		if (saturation)
+			applySaturation(currentCcm_.value(), saturation.value());
+		lastCt_ = ct;
+		lastSaturation_ = saturation;
+		context.activeState.matrixChanged = true;
 	}
 
-	lastCt_ = ct;
-	lastSaturation_ = saturation;
-	Matrix<float, 3, 3> ccm = ccm_.getInterpolated(ct);
-	if (saturation)
-		applySaturation(ccm, saturation.value());
-
-	context.activeState.combinedMatrix = ccm;
-	context.activeState.ccm = ccm;
+	context.activeState.combinedMatrix =
+		currentCcm_.value() * context.activeState.combinedMatrix;
 	frameContext.saturation = saturation;
-	context.activeState.matrixChanged = true;
-	frameContext.ccm = ccm;
+	frameContext.ccm = currentCcm_.value();
 }
 
 void Ccm::process([[maybe_unused]] IPAContext &context,

src/ipa/simple/algorithms/ccm.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 /*
- * Copyright (C) 2024-2025, Red Hat Inc.
+ * Copyright (C) 2024-2026, Red Hat Inc.
  *
  * Color correction matrix
  */
@@ -47,6 +47,7 @@ private:
 	unsigned int lastCt_;
 	std::optional<float> lastSaturation_;
 	Interpolator<Matrix<float, 3, 3>> ccm_;
+	std::optional<Matrix<float, 3, 3>> currentCcm_;
 };
 
 } /* namespace ipa::soft::algorithms */

src/ipa/simple/ipa_context.h
@@ -62,7 +62,6 @@ struct IPAActiveState {
 		double contrastExp;
 	} gamma;
 
-	Matrix<float, 3, 3> ccm;
 	Matrix<float, 3, 3> combinedMatrix;
 	bool matrixChanged = false;
 

src/ipa/simple/soft_simple.cpp
@@ -282,6 +282,8 @@ void IPASoftSimple::queueRequest(const uint32_t frame, const ControlList &contro
 
 void IPASoftSimple::computeParams(const uint32_t frame)
 {
+	context_.activeState.combinedMatrix = Matrix<float, 3, 3>::identity();
+
 	IPAFrameContext &frameContext = context_.frameContexts.get(frame);
 	for (auto const &algo : algorithms())
 		algo->prepare(context_, frame, frameContext, params_);

CodeLinaro-mirror/le_external_libcamera_libcamera

commit c64401949738011a07930a03cf7b46bd05b1ce7a

ipa: simple: Fix Ccm matrix multiplication
The CCM should be applied to the current combined Matrix. Fix the
multiplication ordering.

Fixes: 82ed6c19c2b3 ("libcamera: ipa: simple: Initialise the general correction matrix")
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/ipa/simple/algorithms/ccm.cpp
@@ -54,7 +54,7 @@ void Ccm::prepare(IPAContext &context, [[maybe_unused]] const uint32_t frame,
 	}
 
 	context.activeState.combinedMatrix =
-		currentCcm_.value() * context.activeState.combinedMatrix;
+		context.activeState.combinedMatrix * currentCcm_.value();
 	frameContext.ccm = currentCcm_.value();
 }
 