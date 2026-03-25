b8a43b66c7a5e493013559ed4c3bd99c612879bd
Stefan Klug <stefan.klug@ideasonboard.com>
2026-03-25 16:13:55 +0100

    libcamera: internal: Add SequenceSyncHelper class
    On a V4L2 buffer the assigned sequence is not known until the buffer is
    dequeued. But for per frame controls we have to prepare other data like
    sensor controls and ISP params in advance. So we try to anticipate the
    sequence number a given buffer will be. In a perfect world this works
    well as long as the initial sequence is assigned correctly. But it
    breaks as soon as things like running out of buffers or incomplete
    images happen. To make things even more complicated, in most cases
    more than one buffer is queued to the kernel at a time.
    
    So as soon as a sequence number doesn't match the expected one after
    dequeuing, most likely all the already queued buffers will be dequeued
    with the same error.  It is not sufficient to simply add the correction
    after dequeuing because the error on all queued frames would accumulate
    and the whole system starts to oscillate. To work around that add a
    SequenceSyncHelper class that tracks the expected error and allows to
    easily query the necessary correction when queuing new buffers.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>

---
+++ src/libcamera/sequence_sync_helper.cpp
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2026, Ideas on Board.
+ *
+ * Helper to easily record debug metadata inside libcamera.
+ */
+
+#include "libcamera/internal/sequence_sync_helper.h"
+
+#include <libcamera/base/log.h>
+
+namespace libcamera {
+
+LOG_DEFINE_CATEGORY(SequenceSyncHelper)
+
+/**
+ * \file debug_controls.h
+ * \brief Helper to synchronize buffer sequences
+ */
+
+} /* namespace libcamera */
