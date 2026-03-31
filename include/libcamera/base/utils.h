355e9f81648495eb058fffd985207feb176a98ac
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2026-03-31 18:36:30 +0200

    libcamera: utils: Add overloaded visitor helpers
    std::visit() allows quite elegant type-matching implementation of the
    visitor pattern.
    
    The 'overloaded' type helpers allow to define a hierarchy of overloaded
    operator() implementations which can be used by std::visit().
    
    Currently only the Virtual pipeline handler uses this type-matching
    implementation of std::visit. To prepare to add another user in the Mali
    C55 pipeline handler move the 'overloaded' helper type to
    libcamera::utils for easier re-use.
    
    Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- include/libcamera/base/utils.h
+++ include/libcamera/base/utils.h
@@ -37,6 +37,13 @@ namespace libcamera {
 
 namespace utils {
 
+template<class... Ts>
+struct overloaded : Ts... {
+	using Ts::operator()...;
+};
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
 const char *basename(const char *path);
 
 char *secure_getenv(const char *name);
