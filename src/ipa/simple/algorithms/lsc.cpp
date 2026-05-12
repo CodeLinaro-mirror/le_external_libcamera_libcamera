5a2b641307af0658b7bc038c1d11172cb586a9af
Xander Pronk <xander.c.pronk@gmail.com>
2026-05-12 14:36:08 +0200

    ipa: simple: Add LSC algorithm
    The lens shading algorithm uses grid-based interpolation.  The grid
    values are specified in a tuning file as multiplication factors for each
    of the RGB colours.
    
    The interpolated grid is transformed to an array to be used as an RGB
    texture in debayering.
    
    Co-developed-by: Rick ten Wolde <rick_libcamera@wolde.info>
    Signed-off-by: Rick ten Wolde <rick_libcamera@wolde.info>
    Signed-off-by: Xander Pronk <xander.c.pronk@gmail.com>
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/ipa/simple/algorithms/lsc.cpp
@@ -0,0 +1,63 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Lens shading correction
+ */
+
+#include "lsc.h"
+
+#include <libcamera/base/log.h>
+
+#include "awb.h"
+
+namespace libcamera {
+
+namespace ipa::soft::algorithms {
+
+LOG_DEFINE_CATEGORY(IPASoftLsc)
+
+int Lsc::init([[maybe_unused]] IPAContext &context, const ValueNode &tuningData)
+{
+	int retR = lscR_.readYaml(tuningData["sets"], "ct", "r");
+	int retG = lscG_.readYaml(tuningData["sets"], "ct", "g");
+	int retB = lscB_.readYaml(tuningData["sets"], "ct", "b");
+
+	if (retR < 0 || retG < 0 || retB < 0) {
+		LOG(IPASoftLsc, Error)
+			<< "Failed to parse 'lsc' parameter from tuning file.";
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+int Lsc::configure([[maybe_unused]] IPAContext &context,
+		   [[maybe_unused]] const IPAConfigInfo &configInfo)
+{
+	return 0;
+}
+
+void Lsc::prepare(IPAContext &context, [[maybe_unused]] const uint32_t frame,
+		  [[maybe_unused]] IPAFrameContext &frameContext, DebayerParams *params)
+{
+	unsigned int ct =
+		context.activeState.awb.temperatureK.value_or(kDefaultTemperature);
+
+	const LscMatrix matrixR = lscR_.getInterpolated(ct);
+	const LscMatrix matrixG = lscG_.getInterpolated(ct);
+	const LscMatrix matrixB = lscB_.getInterpolated(ct);
+
+	DebayerParams::LscLookupTable lut;
+	constexpr unsigned int gridSize = DebayerParams::kLscGridSize;
+	for (unsigned int i = 0, j = 0; i < gridSize * gridSize; i++) {
+		lut[j++] = matrixR.data()[i];
+		lut[j++] = matrixG.data()[i];
+		lut[j++] = matrixB.data()[i];
+	}
+	params->lscLut = lut;
+}
+
+REGISTER_IPA_ALGORITHM(Lsc, "Lsc")
+
+} /* namespace ipa::soft::algorithms */
+
+} /* namespace libcamera */
---
+++ src/ipa/simple/algorithms/lsc.h
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Lens shading correction
+ */
+
+#pragma once
+
+#include "libcamera/internal/matrix.h"
+
+#include <libipa/interpolator.h>
+
+#include "algorithm.h"
+
+namespace libcamera {
+
+namespace ipa::soft::algorithms {
+
+class Lsc : public Algorithm
+{
+public:
+	Lsc() = default;
+	~Lsc() = default;
+
+	int init(IPAContext &context, const ValueNode &tuningData) override;
+	int configure(IPAContext &context,
+		      const IPAConfigInfo &configInfo) override;
+	void prepare(IPAContext &context,
+		     const uint32_t frame,
+		     IPAFrameContext &frameContext,
+		     DebayerParams *params) override;
+
+private:
+	using LscMatrix = Matrix<float, DebayerParams::kLscGridSize, DebayerParams::kLscGridSize>;
+	Interpolator<LscMatrix> lscR_;
+	Interpolator<LscMatrix> lscG_;
+	Interpolator<LscMatrix> lscB_;
+};
+
+} /* namespace ipa::soft::algorithms */
+
+} /* namespace libcamera */
