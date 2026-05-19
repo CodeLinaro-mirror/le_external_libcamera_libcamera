10c503495a86a9515cda75d889b38702b04a0cc4
Kate Hsuan <hpa@redhat.com>
2026-05-19 11:00:20 +0800

    meson: Add ipa-signature-algo option
    Add a combo type "ipa-signature-algo" meson option to select signature
    algorithms, including rsa-sha256 and ml-dsa-65.
    ras-sha256 is the default setting for now.
    
    Signed-off-by: Kate Hsuan <hpa@redhat.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- utils/gen-ipa-priv-key.sh
+++ utils/gen-ipa-priv-key.sh
@@ -6,6 +6,18 @@
 #
 # Generate an RSA private key to sign IPA modules
 
-key="$1"
+algo="$1"
+key="$2"
 
-openssl genpkey -algorithm RSA -out "${key}" -pkeyopt rsa_keygen_bits:2048
+# Two possible algorithms: RSA and ML-DSA-65
+# openssl genpkey -algorithm RSA -out "${key}" -pkeyopt rsa_keygen_bits:2048
+# openssl genpkey -algorithm ML-DSA-65 -out "${key}"
+
+if [ "$algo" = "RSA" ]; then
+    openssl genpkey -algorithm RSA -out "${key}" -pkeyopt rsa_keygen_bits:2048
+elif [ "$algo" = "ML-DSA-65" ]; then
+    openssl genpkey -algorithm ML-DSA-65 -out "${key}"
+else
+    echo "Invalid algorithm: $algo"
+    exit 1
+fi
