da417169f3d3bf32df42c7127d76ad6ef6035bc0
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2026-06-15 16:05:27 +0200

    ipa: mali-c55: awb: Port to use libipa AwbAlgorithm
    Port the Mali-C55 Awb algorithm to use the new libipa implementation
    of AwbAlgorithm.
    
    The awbAlgo_ class member is initialized with the Q<4, 8> type that
    represents the register format and the MaliC55AwbStats type handles
    the WB statistics format as produced by the Mali-C55 which already
    calculates the mean values as R/G and B/G values.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/mali-c55/ipa_context.h
+++ src/ipa/mali-c55/ipa_context.h
@@ -12,8 +12,10 @@
 
 #include "libcamera/internal/bayer_format.h"
 
+#include <libipa/camera_sensor_helper.h>
 #include <libipa/fc_queue.h>
 
+#include "libipa/awb.h"
 #include "libipa/fixedpoint.h"
 
 namespace libcamera {
@@ -28,6 +30,8 @@ struct IPASessionConfiguration {
 		double minAnalogueGain;
 		double maxAnalogueGain;
 	} agc;
+
+	ipa::awb::Session awb;
 
 	struct {
 		BayerFormat::Order bayerOrder;
@@ -54,10 +58,7 @@ struct IPAActiveState {
 		uint32_t temperatureK;
 	} agc;
 
-	struct {
-		UQ<4, 8> rGain;
-		UQ<4, 8> bGain;
-	} awb;
+	ipa::awb::ActiveState awb;
 };
 
 struct IPAFrameContext : public FrameContext {
@@ -67,10 +68,7 @@ struct IPAFrameContext : public FrameContext {
 		UQ<5, 8> ispGain;
 	} agc;
 
-	struct {
-		UQ<4, 8> rGain;
-		UQ<4, 8> bGain;
-	} awb;
+	ipa::awb::FrameContext awb;
 };
 
 struct IPAContext {
@@ -85,6 +83,9 @@ struct IPAContext {
 	FCQueue<IPAFrameContext> frameContexts;
 
 	ControlInfoMap::Map ctrlMap;
+
+	/* Interface to the Camera Helper */
+	std::unique_ptr<CameraSensorHelper> camHelper;
 };
 
 } /* namespace ipa::mali_c55 */
