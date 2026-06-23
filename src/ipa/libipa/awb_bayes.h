e35eafacbf6e89e3e0e0b118bd95c2e5fd020095
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2026-06-23 15:54:56 +0200

    ipa: libipa: awb: Reimplement AwbAlgorithm
    The current implementation of AwbAlgorithm is the following one:
    
    AwbAlgorithm o---- RkISP1Awb
        ^
        |
     |-----|
     |     |
    Bayes Grey
    
    The platform-specific Awb implementation instantiate one of
    the Bayes or Grey Awb helper classes and both inherit from the
    AwbAlgorithm base class.
    
    The handling of libcamera controls is split between the base and the
    derived (mostly Bayes) class.
    
    We want instead the AwbAlgorithm to handle all-things-libcamera and let
    the Bayes and Grey classes implement the logic. In order to achieve that
    use composition also in the AwbAlgorithm class and introduce an
    AwbImplementation base class from which Bayes and Grey derive from.
    
    AwbImplementation o---- AwbAlgorithmBase
        ^                           ^
        |                           |
     |-----|                AwbAlgorithm<Q<x,y>> o---- RkISP1Awb
     |     |
    Bayes Grey
    
    The AwbAlgorithm<> class is instantiated with a Q<x, y> template
    argument that represents the platform-specific register format which
    is used to initialize controls and clamp gains to the hardware limits.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>
    Tested-by: Daniel Scally <dan.scally@ideasonboard.com>

--- src/ipa/libipa/awb_bayes.h
+++ src/ipa/libipa/awb_bayes.h
@@ -20,22 +20,23 @@ namespace libcamera {
 
 namespace ipa {
 
-class AwbBayes : public AwbAlgorithm
+class AwbBayes : public AwbImplementation
 {
 public:
 	AwbBayes() = default;
 
 	int init(const ValueNode &tuningData) override;
-	AwbResult calculateAwb(const AwbStats &stats, unsigned int lux) override;
+	awb::Context calculateAwb(const AwbStats &stats, unsigned int lux,
+				  std::array<double, 2> ranges) override;
 	std::optional<RGB<double>> gainsFromColourTemperature(double temperatureK) override;
-	void handleControls(const ControlList &controls) override;
 
 private:
 	int readPriors(const ValueNode &tuningData);
 
 	void fineSearch(double &t, double &r, double &b, ipa::Pwl const &prior,
 			const AwbStats &stats) const;
-	double coarseSearch(const ipa::Pwl &prior, const AwbStats &stats) const;
+	double coarseSearch(const ipa::Pwl &prior, const AwbStats &stats,
+			    std::array<double, 2> ranges) const;
 	double interpolateQuadratic(ipa::Pwl::Point const &a,
 				    ipa::Pwl::Point const &b,
 				    ipa::Pwl::Point const &c) const;
@@ -50,8 +51,6 @@ private:
 
 	double transversePos_;
 	double transverseNeg_;
-
-	ModeConfig *currentMode_ = nullptr;
 };
 
 } /* namespace ipa */
--- src/ipa/libipa/awb_grey.cpp
+++ src/ipa/libipa/awb_grey.cpp
@@ -60,6 +60,7 @@ int AwbGrey::init(const ValueNode &tuningData)
  * \brief Calculate AWB data from the given statistics
  * \param[in] stats The statistics to use for the calculation
  * \param[in] lux The lux value of the scene
+ * \param[in] ranges The colour temperature search limits (AwbBayes only)
  *
  * The colour temperature is estimated based on the colours::estimateCCT()
  * function. The gains are calculated purely based on the RGB means provided by
@@ -70,20 +71,23 @@ int AwbGrey::init(const ValueNode &tuningData)
  *
  * \return The AWB result
  */
-AwbResult AwbGrey::calculateAwb(const AwbStats &stats, [[maybe_unused]] unsigned int lux)
+awb::Context
+AwbGrey::calculateAwb(const AwbStats &stats, [[maybe_unused]] unsigned int lux,
+		      [[maybe_unused]] std::array<double, 2> ranges)
 {
-	AwbResult result;
+	awb::Context result;
 	auto means = stats.rgbMeans();
-	result.colourTemperature = estimateCCT(means);
+	result.temperatureK = estimateCCT(means);
 
 	/*
-	 * Estimate the red and blue gains to apply in a grey world. The green
-	 * gain is hardcoded to 1.0. Avoid divisions by zero by clamping the
-	 * divisor to a minimum value of 1.0.
+	 * Calculate the red and blue gains to apply in a grey world by simply
+	 * inverting the red/green and blue/green ratios as reported in
+	 * statistics. The green gain is hardcoded to 1.0.
 	 */
-	result.gains.r() = means.g() / std::max(means.r(), 1.0);
+	result.gains.r() = 1.0 / stats.rgRatio();
 	result.gains.g() = 1.0;
-	result.gains.b() = means.g() / std::max(means.b(), 1.0);
+	result.gains.b() = 1.0 / stats.bgRatio();
+
 	return result;
 }
 
@@ -96,12 +100,13 @@ AwbResult AwbGrey::calculateAwb(const AwbStats &stats, [[maybe_unused]] unsigned
  * gains configured in the colour temperature curve.
  *
  * \return The colour gains if a colour temperature curve is available,
- * [1, 1, 1] otherwise.
+ * std::nullopt otherwise
  */
 std::optional<RGB<double>> AwbGrey::gainsFromColourTemperature(double colourTemperature)
 {
 	if (!colourGainCurve_) {
-		LOG(Awb, Error) << "No gains defined";
+		LOG(Awb, Info) << "No gains curve defined, "
+			       << "unable to interpolate gains to colour temperature";
 		return std::nullopt;
 	}
 
