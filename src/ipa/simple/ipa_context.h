65d422983c075fd37b797bd3160d5d6d079c09af
Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
2026-07-03 17:38:05 +0200

    ipa: simple: agc: Do not overwrite sensor exposure/gain
    In the agc algorithm do not overwrite the sensor exposure/gain values,
    instead add a new "agc" member to the frame context and update that.
    
    No functional changes are intended as the agc algorithm runs last.
    
    Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/simple/ipa_context.h
+++ src/ipa/simple/ipa_context.h
@@ -69,6 +69,11 @@ struct IPAFrameContext : public FrameContext {
 	struct {
 		int32_t exposure;
 		double gain;
+	} agc;
+
+	struct {
+		int32_t exposure;
+		double gain;
 	} sensor;
 
 	RGB<float> gains;
