e406045a5f6802a03a71b809bbfeeb36f39127fe
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2026-07-08 17:51:03 +0200

    ipa: libipa: lsc: Introduce lsc::ActiveState
    Introduce the lsc.h and lsc.cpp files with the definition of the
    Lsc algorithm active state.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/ipa/libipa/lsc.cpp
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2026 Ideas on Board Oy
+ *
+ * libIPA Lsc algorithms
+ */
+
+#include "lsc.h"
+
+/**
+ * \file lsc.h
+ * \brief libipa LSC algorithm
+ */
+
+namespace libcamera {
+
+namespace ipa {
+
+namespace lsc {
+
+/**
+ * \struct ActiveState
+ * \brief The LSC active state
+ *
+ * \var ActiveState::enabled
+ * \brief Boolean flag for the LscAlgorithm enable status
+ */
+
+} /* namespace lsc */
+
+} /* namespace ipa */
+
+} /* namespace libcamera */
