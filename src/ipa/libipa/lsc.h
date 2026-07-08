42fe4c43e0703e2a2f6898e170cd346b3b6adf89
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2026-07-08 11:25:09 +0200

    ipa: libipa: lsc: Introduce LscAlgorithm
    Introduce the LscAlgorithm class that implements the LSC algorithm
    for libipa.
    
    The class uses two backends (LscPolynomial and LscTable) to perform
    tuning file parsing and re-scaling of the LSC gains.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/libipa/lsc.h
+++ src/ipa/libipa/lsc.h
@@ -7,6 +7,17 @@
 
 #pragma once
 
+#include <memory>
+#include <vector>
+
+#include <libcamera/controls.h>
+#include <libcamera/geometry.h>
+
+#include "libcamera/internal/value_node.h"
+
+#include "interpolator.h"
+#include "lsc_base.h"
+
 namespace libcamera {
 
 namespace ipa {
@@ -23,6 +34,37 @@ struct FrameContext {
 };
 
 } /* namespace lsc */
+
+class LscAlgorithm
+{
+public:
+	int init(const ValueNode &tuningData, const Size &sensorSize,
+		 ControlInfoMap::Map &controls);
+
+	int configure(lsc::ActiveState &state, const Rectangle &analogCrop,
+		      const std::vector<double> &xPos,
+		      const std::vector<double> &yPos);
+
+	void queueRequest(lsc::ActiveState &state, lsc::FrameContext &context,
+			  const ControlList &controls);
+	void process(lsc::FrameContext &context, ControlList &metadata);
+
+	const lsc::Components interpolateComponents(unsigned int ct)
+	{
+		return sets_.getInterpolated(ct);
+	}
+
+	const lsc::ComponentsMap getComponents()
+	{
+		return lscData_;
+	}
+
+private:
+	std::unique_ptr<LscImplementation> impl_;
+	Interpolator<lsc::Components> sets_;
+	lsc::ComponentsMap lscData_;
+	bool polynomial_;
+};
 
 } /* namespace ipa */
 
