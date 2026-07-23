da3af38e848064b684ef099eb2b28ebca33e472f
Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
2026-07-23 17:43:04 +0200

    ipa: libipa: Add `AgcAlgorithm`
    Add a class that implements the `Algorithm` interface using `AgcMeanLuminance`
    based on the rkisp1 `Agc` algorithm, with slight adjustments.
    
    Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/libipa/agc.h
+++ src/ipa/libipa/agc.h
@@ -7,13 +7,19 @@
 
 #pragma once
 
+#include <optional>
 #include <utility>
 
 #include <linux/v4l2-controls.h>
 
+#include <libcamera/control_ids.h>
 #include <libcamera/controls.h>
 
+#include <libcamera/ipa/core_ipa_interface.h>
+
+#include "agc_mean_luminance.h"
 #include "camera_sensor_helper.h"
+#include "histogram.h"
 
 namespace libcamera {
 
@@ -42,7 +48,99 @@ prepareControls(ControlList &controls, const CameraSensorHelper *sensor,
 	controls.set(V4L2_CID_ANALOGUE_GAIN, int32_t(sensor ? sensor->gainCode(gain) : gain));
 }
 
+struct Session {
+	utils::Duration minExposureTime;
+	utils::Duration maxExposureTime;
+	double minAnalogueGain;
+	double maxAnalogueGain;
+	utils::Duration minFrameDuration;
+	utils::Duration maxFrameDuration;
+
+	utils::Duration lineDuration;
+
+	struct {
+		Size outputSize;
+	} sensor;
+
+	bool autoAllowed;
+};
+
+struct ActiveState {
+	struct {
+		uint32_t exposure;
+		double gain;
+	} manual;
+	struct {
+		uint32_t exposure;
+		double gain;
+		double quantizationGain;
+		double yTarget;
+	} automatic;
+
+	bool autoExposureEnabled;
+	bool autoGainEnabled;
+	double exposureValue;
+	controls::AeConstraintModeEnum constraintMode;
+	controls::AeExposureModeEnum exposureMode;
+	utils::Duration minFrameDuration;
+	utils::Duration maxFrameDuration;
+};
+
+struct FrameContext {
+	uint32_t exposure;
+	double gain;
+	double quantizationGain;
+	double exposureValue;
+	double yTarget;
+	uint32_t vblank;
+	bool autoExposureEnabled;
+	bool autoGainEnabled;
+	controls::AeConstraintModeEnum constraintMode;
+	controls::AeExposureModeEnum exposureMode;
+	utils::Duration minFrameDuration;
+	utils::Duration maxFrameDuration;
+	utils::Duration frameDuration;
+	bool autoExposureModeChange;
+	bool autoGainModeChange;
+};
+
 } /* namespace agc */
+
+class AgcAlgorithm
+{
+public:
+	struct ConfigurationParams {
+		const CameraSensorHelper *sensor;
+		const IPACameraSensorInfo &sensorInfo;
+		const ControlInfoMap &sensorControls;
+		ControlInfoMap::Map &ctrlMap;
+		bool autoAllowed = true;
+	};
+
+	int init(const ValueNode &tuningData);
+
+	int configure(agc::Session &session, agc::ActiveState &state, const ConfigurationParams &config);
+
+	void queueRequest(const agc::Session &session, agc::ActiveState &state,
+			  agc::FrameContext &frameContext, const ControlList &controls);
+
+	void prepare(agc::ActiveState &state, agc::FrameContext &frameContext);
+
+	struct ProcessParams {
+		const AgcMeanLuminance::Traits &traits;
+		const Histogram &yHist;
+		uint32_t exposure;
+		double gain;
+		std::vector<AgcMeanLuminance::AgcConstraint> &&additionalConstraints = {};
+		double lux = 0;
+	};
+
+	void process(const agc::Session &session, agc::ActiveState &state, agc::FrameContext &frameContext,
+		     std::optional<ProcessParams> &&params, ControlList &metadata);
+
+private:
+	AgcMeanLuminance impl_;
+};
 
 } /* namespace ipa */
 
