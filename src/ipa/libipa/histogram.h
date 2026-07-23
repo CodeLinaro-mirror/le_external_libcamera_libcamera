4e1ee425a4c66309aee44b6db07190c6b6b731f4
Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
2026-07-23 17:43:14 +0200

    ipa: libipa: histogram: Add `operator[]` to query bin
    Add an `operator[]` that takes an integral bin number and returns
    the count pertaining to that specific bin.
    
    Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/libipa/histogram.h
+++ src/ipa/libipa/histogram.h
@@ -42,6 +42,13 @@ public:
 	double quantile(double q, uint32_t first = 0, uint32_t last = UINT_MAX) const;
 	double interQuantileMean(double lowQuantile, double hiQuantile) const;
 
+	uint32_t operator[](size_t bin) const
+	{
+		return bin < bins()
+			? cumulative_[bin + 1] - cumulative_[bin]
+			: 0;
+	}
+
 private:
 	std::vector<uint64_t> cumulative_;
 };
