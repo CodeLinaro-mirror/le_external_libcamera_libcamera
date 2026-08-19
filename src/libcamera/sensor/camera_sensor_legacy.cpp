47ceb1e07413563ff25516e9d917720f273a7e04
Stefan Klug <stefan.klug@ideasonboard.com>
2026-08-19 16:15:16 +0200

    libcamera: ipa: core: Add IPACameraSensorInfo::pixelArraySize property
    Add a IPACameraSensorInfo::pixelArraySize property to communicate the
    full size of the pixel array to the IPA. This property is not used today
    but will be useful to do sanity checks on coordinates handled in the
    algorithms.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Reviewed-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/sensor/camera_sensor_legacy.cpp
+++ src/libcamera/sensor/camera_sensor_legacy.cpp
@@ -881,6 +881,7 @@ int CameraSensorLegacy::sensorInfo(IPACameraSensorInfo *info) const
 		return -EINVAL;
 
 	info->model = model();
+	info->pixelArraySize = pixelArraySize_;
 
 	/*
 	 * The active area is a static property, while the crop rectangle needs
