kbingham/libcamera

commit d780e285b593dd12e950777c13f69ce0799981e1

shaders: bayer: Use native matrix multiplication
Remove the open coded matrix multiplication as glsl can perform this
directly. Adapt the uniform ccmUniformDataIn_ to ensure we correctly
upload the matrix in row major ordering.

Tested-by: Robert Mader <robert.mader@collabora.com>
Tested-by: Bryan O'Donoghue <bod.linux@nxsw.ie>
Reviewed-by: Bryan O'Donoghue <bod.linux@nxsw.ie>
Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/software_isp/debayer_egl.cpp
@@ -486,18 +486,7 @@ void DebayerEGL::setShaderVariableValues(const DebayerParams &params)
 			    << " textureUniformStrideFactor_ " << Stride
 			    << " textureUniformProjMatrix_ " << textureUniformProjMatrix_;
 
-	GLfloat ccm[9] = {
-		params.combinedMatrix[0][0],
-		params.combinedMatrix[0][1],
-		params.combinedMatrix[0][2],
-		params.combinedMatrix[1][0],
-		params.combinedMatrix[1][1],
-		params.combinedMatrix[1][2],
-		params.combinedMatrix[2][0],
-		params.combinedMatrix[2][1],
-		params.combinedMatrix[2][2],
-	};
-	glUniformMatrix3fv(ccmUniformDataIn_, 1, GL_FALSE, ccm);
+	glUniformMatrix3fv(ccmUniformDataIn_, 1, GL_TRUE, params.combinedMatrix.data().data());
 	LOG(Debayer, Debug) << " ccmUniformDataIn_ " << ccmUniformDataIn_ << " data " << params.combinedMatrix;
 
 	/*

src/libcamera/shaders/bayer_1x_packed.frag
@@ -235,49 +235,8 @@ void main(void)
 	/* Apply AWB gains, and saturate each channel at sensor range */
 	rgb = clamp(rgb * awb, vec3(0.0), vec3(1.0));
 
-	/*
-	 *   CCM is a 3x3 in the format
-	 *
-	 *   +--------------+----------------+---------------+
-	 *   | RedRedGain   | RedGreenGain   | RedBlueGain   |
-	 *   +--------------+----------------+---------------+
-	 *   | GreenRedGain | GreenGreenGain | GreenBlueGain |
-	 *   +--------------+----------------+---------------+
-	 *   | BlueRedGain  |  BlueGreenGain | BlueBlueGain  |
-	 *   +--------------+----------------+---------------+
-	 *
-	 *   Rout = RedRedGain * Rin + RedGreenGain * Gin + RedBlueGain * Bin
-	 *   Gout = GreenRedGain * Rin + GreenGreenGain * Gin + GreenBlueGain * Bin
-	 *   Bout = BlueRedGain * Rin + BlueGreenGain * Gin + BlueBlueGain * Bin
-	 *
-	 *   We upload to the GPU without transposition glUniformMatrix3f(.., .., GL_FALSE, ccm);
-	 *
-	 *   CPU
-	 *   float ccm [] = {
-	 *             RedRedGain,   RedGreenGain,   RedBlueGain,
-	 *             GreenRedGain, GreenGreenGain, GreenBlueGain,
-	 *             BlueRedGain,  BlueGreenGain,  BlueBlueGain,
-	 *   };
-	 *
-	 *   GPU
-	 *   ccm = {
-	 *             RedRedGain,   GreenRedGain,   BlueRedGain,
-	 *             RedGreenGain, GreenGreenGain, BlueGreenGain,
-	 *             RedBlueGain,  GreenBlueGain,  BlueBlueGain,
-	 *   }
-	 *
-	 *   However the indexing for the mat data-type is column major hence
-	 *   ccm[0][0] = RedRedGain, ccm[0][1] = RedGreenGain, ccm[0][2] = RedBlueGain
-	 *
-	 */
-	float rin, gin, bin;
-	rin = rgb.r;
-	gin = rgb.g;
-	bin = rgb.b;
-
-	rgb.r = (rin * ccm[0][0]) + (gin * ccm[0][1]) + (bin * ccm[0][2]);
-	rgb.g = (rin * ccm[1][0]) + (gin * ccm[1][1]) + (bin * ccm[1][2]);
-	rgb.b = (rin * ccm[2][0]) + (gin * ccm[2][1]) + (bin * ccm[2][2]);
+	/* Colour Correction Matrix */
+	rgb = ccm * rgb;
 
 	/*
 	 * Contrast

src/libcamera/shaders/bayer_unpacked.frag
@@ -138,49 +138,8 @@ void main(void) {
     /* Apply AWB gains, and saturate each channel at sensor range */
     rgb = clamp(rgb * awb, vec3(0.0), vec3(1.0));
 
-    /*
-     *   CCM is a 3x3 in the format
-     *
-     *   +--------------+----------------+---------------+
-     *   | RedRedGain   | RedGreenGain   | RedBlueGain   |
-     *   +--------------+----------------+---------------+
-     *   | GreenRedGain | GreenGreenGain | GreenBlueGain |
-     *   +--------------+----------------+---------------+
-     *   | BlueRedGain  |  BlueGreenGain | BlueBlueGain  |
-     *   +--------------+----------------+---------------+
-     *
-     *   Rout = RedRedGain * Rin + RedGreenGain * Gin + RedBlueGain * Bin
-     *   Gout = GreenRedGain * Rin + GreenGreenGain * Gin + GreenBlueGain * Bin
-     *   Bout = BlueRedGain * Rin + BlueGreenGain * Gin + BlueBlueGain * Bin
-     *
-     *   We upload to the GPU without transposition glUniformMatrix3f(.., .., GL_FALSE, ccm);
-     *
-     *   CPU
-     *   float ccm [] = {
-     *             RedRedGain,   RedGreenGain,   RedBlueGain,
-     *             GreenRedGain, GreenGreenGain, GreenBlueGain,
-     *             BlueRedGain,  BlueGreenGain,  BlueBlueGain,
-     *   };
-     *
-     *   GPU
-     *   ccm = {
-     *             RedRedGain,   GreenRedGain,   BlueRedGain,
-     *             RedGreenGain, GreenGreenGain, BlueGreenGain,
-     *             RedBlueGain,  GreenBlueGain,  BlueBlueGain,
-     *   }
-     *
-     *   However the indexing for the mat data-type is column major hence
-     *   ccm[0][0] = RedRedGain, ccm[0][1] = RedGreenGain, ccm[0][2] = RedBlueGain
-     *
-     */
-    float rin, gin, bin;
-    rin = rgb.r;
-    gin = rgb.g;
-    bin = rgb.b;
-
-    rgb.r = (rin * ccm[0][0]) + (gin * ccm[0][1]) + (bin * ccm[0][2]);
-    rgb.g = (rin * ccm[1][0]) + (gin * ccm[1][1]) + (bin * ccm[1][2]);
-    rgb.b = (rin * ccm[2][0]) + (gin * ccm[2][1]) + (bin * ccm[2][2]);
+    /* Colour Correction Matrix */
+    rgb = ccm * rgb;
 
     /*
      * Contrast

CodeLinaro-mirror/le_external_libcamera_libcamera

commit f9f2619eefa8915dec135f7f364f063515be70b0

debayer_egl: Use untransposed matrix to fix GLES 2.0
The softISP curently fails on Mali-400 on Mesa. It turns out that
transposed matrices are not allowed by the spec:
> The UniformMatrix{234}fv commands will load count 2 × 2, 3 × 3, or 4 × 4
> matrices (corresponding to 2, 3, or 4 in the command name) of floating-point values
> into a uniform location defined as a matrix or an array of matrices. The matrix is
> specified in column-major order. transpose must be FALSE.

Thus partially revert the commit mentioned below.

Fixes: d780e285b593 ("shaders: bayer: Use native matrix multiplication")
Signed-off-by: Robert Mader <robert.mader@collabora.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/software_isp/debayer_egl.cpp
@@ -461,7 +461,18 @@ void DebayerEGL::setShaderVariableValues(eGLImage &eglImageIn, const DebayerPara
 			    << " textureUniformStrideFactor_ " << Stride
 			    << " textureUniformProjMatrix_ " << textureUniformProjMatrix_;
 
-	glUniformMatrix3fv(ccmUniformDataIn_, 1, GL_TRUE, params.combinedMatrix.data().data());
+	GLfloat ccm[9] = {
+		params.combinedMatrix[0][0],
+		params.combinedMatrix[0][1],
+		params.combinedMatrix[0][2],
+		params.combinedMatrix[1][0],
+		params.combinedMatrix[1][1],
+		params.combinedMatrix[1][2],
+		params.combinedMatrix[2][0],
+		params.combinedMatrix[2][1],
+		params.combinedMatrix[2][2],
+	};
+	glUniformMatrix3fv(ccmUniformDataIn_, 1, GL_FALSE, ccm);
 	LOG(Debayer, Debug) << " ccmUniformDataIn_ " << ccmUniformDataIn_ << " data " << params.combinedMatrix;
 
 	/*