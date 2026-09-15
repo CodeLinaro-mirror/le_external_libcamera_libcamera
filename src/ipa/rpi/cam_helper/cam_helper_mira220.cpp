kbingham/libcamera

commit b350edae18013824028e69e3306ef9e052185ba4

ipa: rpi: cam_helper: Add Mira220 cam helper
AMS-OSRAM Mira220 is a global shutter sensor with a maximum
resolution of 1600x1400.

Mira220 doesn't support embedded data metadata, and uses delayed controls
which works by counting frames.

Signed-off-by: Philippe Baetens <philippebaetens@gmail.com>
Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
Acked-by: Naushir Patuck <naush@raspberrypi.com>

src/ipa/rpi/cam_helper/cam_helper_mira220.cpp
@@ -1,0 +1,59 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2026, ams-OSRAM
+ *
+ * Camera helper for Mira220 sensor
+ */
+
+#include <assert.h>
+
+#include "cam_helper.h"
+
+using namespace RPiController;
+
+class CamHelperMira220 : public CamHelper
+{
+public:
+	CamHelperMira220();
+	uint32_t gainCode(double gain) const override;
+	double gain(uint32_t gainCode) const override;
+	unsigned int hideFramesModeSwitch() const override;
+
+private:
+	/*
+	 * Smallest difference between the frame length and integration time,
+	 * in units of lines.
+	 *
+	 * The integration diff is expressed as
+	 *    Tframe - 1928 / row_length
+	 *
+	 * row_length controls the line timings and varies according to the
+	 * number of data lanes in use and the D-PHY data rate. Use an
+	 * integration diff calculated using the value of 304, which represents
+	 * the minimum row_length for a 2 data lanes configuration running at
+	 * 1.5Gbps.
+	 */
+	static constexpr int frameIntegrationDiff = 6;
+};
+
+CamHelperMira220::CamHelperMira220()
+	: CamHelper({}, frameIntegrationDiff)
+{
+}
+
+uint32_t CamHelperMira220::gainCode(double gain) const
+{
+	return static_cast<uint32_t>(2048.0 - 2048.0 / gain);
+}
+
+double CamHelperMira220::gain(uint32_t gainCode) const
+{
+	return static_cast<double>(2048.0 / (2048 - gainCode));
+}
+
+static CamHelper *create()
+{
+	return new CamHelperMira220();
+}
+
+static RegisterCamHelper reg("mira220", &create);

src/ipa/rpi/cam_helper/meson.build
@@ -13,6 +13,7 @@ rpi_ipa_cam_helper_sources = files([
     'cam_helper_imx519.cpp',
     'cam_helper_imx678.cpp',
     'cam_helper_imx708.cpp',
+    'cam_helper_mira220.cpp',
     'cam_helper_ov64a40.cpp',
     'cam_helper_ov7251.cpp',
     'cam_helper_ov9281.cpp',

CodeLinaro-mirror/le_external_libcamera_libcamera

commit 50fff8e855c203a685206f776da6aea939cf8252

ipa: rpi: mira220: Drop unused override for hideFramesModeSwitch
The hideFramesModeSwitch override is declared but never defined. Drop
it. This fixes IPA runtime errors seen on Raspberry Pi 4 because of
missing symbols.

If the sensor does require skipping frames during mode switch, it needs
to be investigated and fixed in a separate patch.

Fixes: b350edae1801 ("ipa: rpi: cam_helper: Add Mira220 cam helper")
Signed-off-by: Jai Luthra <jai.luthra@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/ipa/rpi/cam_helper/cam_helper_mira220.cpp
@@ -17,7 +17,6 @@ public:
 	CamHelperMira220();
 	uint32_t gainCode(double gain) const override;
 	double gain(uint32_t gainCode) const override;
-	unsigned int hideFramesModeSwitch() const override;
 
 private:
 	/*